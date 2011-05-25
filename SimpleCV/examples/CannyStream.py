a9bae785674238859b78d8fe77ec94e2e0510915
Nate Oostendorp <oostendo@chuck.(none)>
2011-05-25 15:40:06 -0400

    first draft of canny stream demo

---
+++ SimpleCV/examples/CannyStream.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python 
+from SimpleCV import *
+
+import sys, curses
+
+#start values for the edges function
+
+def main(stdscr):
+  stdscr.nodelay(1)
+  t1 = 50 
+  t2 = 100 
+  cam = Camera()
+  js = JpegStreamer()
+
+  while (1):
+    c = stdscr.getch() 
+    if (c != -1):
+        if (c == 259):
+          t1 += 5
+        elif (c == 258):
+          t1 -= 5
+        elif (c == 260):
+          t2 -= 5
+        elif (c == 261):
+          t2 += 5
+
+	if (t1 < 0):
+          t1 = 0
+        if (t2 < 0):
+          t2 = 0
+
+        stdscr.addstr(str(int(c)))
+	stdscr.addstr("t1: " + str(t1) + ", t2 "+ str(t2) + "\n")
+        stdscr.refresh()
+	stdscr.move(0, 0)
+
+    cam.getImage().edges(t1, t2).save(js.framebuffer)
+		 
+
+
+
+if __name__ == '__main__':
+  curses.wrapper(main)
