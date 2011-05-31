0342677040272cb593dac76a366c4d1cd67fa113
Nate Oostendorp <oostendo@chuck.(none)>
2011-05-30 22:00:59 -0400

    clean up unused jpegstreamer

--- SimpleCV/examples/simplecam.py
+++ SimpleCV/examples/simplecam.py
@@ -5,19 +5,15 @@ sys.path.append("..")
 
 from SimpleCV import *
 
-
 #settings for the project
 port_original = 8080  #port to view the camera viwe
-port_processed = 8081 #port to look at the processed view
 
 #create JPEG streamers
 original_js = JpegStreamer(port_original)
-processed_js = JpegStreamer(port_processed)
 cam = Camera()
 
 
 while (1):
   i = cam.getImage()
   i.save(original_js.framebuffer)
-  i.save(processed_js.framebuffer)
-  time.sleep(0.2) #yield to the webserver
+  time.sleep(0.01) #yield to the webserver
