7cd9eba23c09b2d93a4ba191aa1eedca09d8b68f
oostendo <nate@oostendorp.net>
2011-06-01 11:22:03 -0400

    can't seem to get the color curve comment to render in autodoc.  annoying

--- SimpleCV/SimpleCV.py
+++ SimpleCV/SimpleCV.py
@@ -29,25 +29,7 @@ import scipy.spatial.distance as spsd
 from numpy import linspace 
 from scipy.interpolate import UnivariateSpline
 
-class ColorCurve:
-  """
-ColorCurve is a color spline class for performing color correction.  It can takeas parameters a SciPy Univariate spline, or an array with at least 4 point pairs.  Either of these must map in a 255x255 space.  The curve can then be used in the applyRGBCurve, applyHSVCurve, and applyInstensityCurve functions::
-
-    clr = ColorCurve([[0,0], [100, 120], [180, 230], [255, 255]])
-    image.applyIntensityCurve(clr)
-
-the only property, mCurve is a linear array with 256 elements from 0 to 255
-  """
-  mCurve =""
-  def __init__(self, curve_vals ):
-    inBins = linspace(0,255,256)
-    if( type(curve_vals) == UnivariateSpline ):
-      self.mCurve = curvVals(inBins)
-    else: 
-      curve_vals = np.array(curve_vals)
-      aSpline = UnivariateSpline( curve_vals[:,0],curve_vals[:,1],s=1)   
-      self.mCurve = aSpline(inBins)
-  
+ 
 #optional libraries
 PIL_ENABLED = True
 try:
@@ -349,7 +331,26 @@ Return the current frame of the JpegStream being monitored
     """
     return Image(pil.open(StringIO(self.camthread.currentframe)))
 
+class ColorCurve:
+  """
+  ColorCurve is a color spline class for performing color correction.  It can takeas parameters a SciPy Univariate spline, or an array with at least 4 point pairs.  Either of these must map in a 255x255 space.  The curve can then be used in the applyRGBCurve, applyHSVCurve, and applyInstensityCurve functions::
 
+    clr = ColorCurve([[0,0], [100, 120], [180, 230], [255, 255]])
+    image.applyIntensityCurve(clr)
+
+  the only property, mCurve is a linear array with 256 elements from 0 to 255
+  """
+  mCurve =""
+
+  def __init__(self, curve_vals ):
+    inBins = linspace(0,255,256)
+    if( type(curve_vals) == UnivariateSpline ):
+      self.mCurve = curvVals(inBins)
+    else: 
+      curve_vals = np.array(curve_vals)
+      aSpline = UnivariateSpline( curve_vals[:,0],curve_vals[:,1],s=1)   
+      self.mCurve = aSpline(inBins)
+ 
 
 class Image:
   """
