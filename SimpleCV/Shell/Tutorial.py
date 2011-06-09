846c877c6f7072d73fb22347f69fa267460f6c73
Anthony Oliver <email@xamox.net>
2011-06-09 11:44:34 -0400

    Added line numbers back into shell for doing macros.  Working more on tutorial

--- SimpleCV/Shell/Tutorial.py
+++ SimpleCV/Shell/Tutorial.py
@@ -27,13 +27,14 @@ class Tutorial():
     print " Welcome to the SimpleCV tutorial "
     print "??????????????????????????????????"
     print lb
-    print "At anytime on the SCV:> you can type tutorial."
+    print "At anytime on the SimpleCV:> you can type tutorial."
     print "then press the tab key and it will autocomplete any tutorial"
     print "that is currently available."
     print lb
     print "You will first learn how to load an image"
-    raw_input("Press enter to continue")
+    raw_input("[Press enter to continue]")
     self.image()
+    self.image_save()
     
     return
     
@@ -47,18 +48,38 @@ class Tutorial():
     print "in this example we have /tmp/simplecv.jpg"
     print "so to load this into memory, it's as easy as:"
     print lb
-    print "img = Image('/tmp/simplecv.jpg')"
+    print "img = Image('./SimpleCV/sampleimages/color.jpg')"
     print lb
-    raw_input("Press enter to continue")
+    raw_input("[Press enter to continue]")
     clear()
     print "Now try it yourself at the terminal"
-    print "img = Image('/tmp/simplecv.jpg')"
+    print "img = Image('./SimpleCV/sampleimages/color.jpg')"
     print lb
-    in_text = raw_input("SimpleCV:>")
-    print in_text
+    in_text = ""
+    shouldbe = "img = Image('./SimpleCV/sampleimages/color.jpg')"
+    while (in_text != shouldbe):
+      in_text = raw_input("SimpleCV:>")
+      if(in_text != shouldbe):
+        print "sorry, that is incorrect"
+        print "please type:"
+        print shouldbe
+
+    clear()
+    print "Correct, you just loaded an image into memory"
+    print "Images are the basis for everything in SimpleCV"
+    print "they are what manipulations are performed on"
+    print "but that is the basis for the image tutorial"
+    print lb
+    raw_input("[Press enter to continue]")
     return
 
   def image_save(self):
+    clear()
+    print "Once you have an Image Object loaded in memory you can"
+    print "now save it to disk.  If you don't or don't know how then"
+    print "you can run the image tutorial"
+    print lb
+    raw_input("[Press enter to continue]")
     clear()
     
     
