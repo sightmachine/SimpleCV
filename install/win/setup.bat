3757c6768efcb80d7c22e734c5ebb477b3890d5a
kscottz <kscott@ingenuitas.com>
2011-06-20 12:13:31 -0400

    windows installer

---
+++ install/win/setup.bat
@@ -0,0 +1,6 @@
+SET PATH=C:/Python27/;C:/Python27/Scripts/;C:/OpenCV2.2/bin/;%PATH%
+SET PYTHONPATH=C:/OpenCV2.2/Python2.7/Lib/site-packages;%PYTHONPATH%
+easy_install http://launchpad.net/pyreadline/trunk/1.7/+download/pyreadline-1.7.zip
+easy_install Imaging-1.1.7
+easy_install nose-1.0.0
+easy_install simplecv-git
