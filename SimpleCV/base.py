0be7e4788ed7052335fed8d4e9b6f868e8f1c04e
Anthony Oliver <anthony@ingenuitas.com>
2011-07-12 12:00:11 -0400

    setup base.py to comply with pylint.  Added lint_test script for testing with correct ignore parameters for pylint

--- SimpleCV/base.py
+++ SimpleCV/base.py
@@ -29,67 +29,89 @@ from scipy.interpolate import UnivariateSpline
 #optional libraries
 PIL_ENABLED = True
 try:
-  import PIL.Image as pil
-  from PIL import ImageFont as pilImageFont
-  from PIL import ImageDraw as pilImageDraw
+    import PIL.Image as pil
+    from PIL import ImageFont as pilImageFont
+    from PIL import ImageDraw as pilImageDraw
 except ImportError:
-  try:
-    import Image as pil  #needed on homebrew on mac
-  except ImportError:
-    PIL_ENABLED = False 
+    try:
+        import Image as pil  #needed on homebrew on mac
+    except ImportError:
+        PIL_ENABLED = False 
 
 BLOBS_ENABLED = True
 try:
-  import cvblob as cvb
+    import cvblob as cvb
 except ImportError:
-  BLOBS_ENABLED = False 
+    BLOBS_ENABLED = False 
 
 ZXING_ENABLED = True
 try:
-  import zxing
+    import zxing
 except ImportError:
-  ZXING_ENABLED = False 
+    ZXING_ENABLED = False 
 
 FREENECT_ENABLED = True
 try:
-  import freenect
+    import freenect
 except ImportError:
-  FREENECT_ENABLED = False 
+    FREENECT_ENABLED = False 
 
 #couple quick typecheck helper functions
 def is_number(n):
-  return type(n) in (IntType, LongType, FloatType)
+    """
+    Determines if it is a number or not
+
+    Returns: Type
+    """
+    return type(n) in (IntType, LongType, FloatType)
 
 def is_tuple(n):
-  return type(n) == tuple 
+    """
+    Determines if it is a tuple or not
+
+    Returns: Boolean
+    """
+    return type(n) == tuple 
 
 def reverse_tuple(n):
-  return tuple(reversed(n))
+    """
+    Reverses a tuple
+
+    Returns: Tuple
+    """
+    return tuple(reversed(n))
 
 def find(f, seq):
-  """Return true if found in list"""
-  for item in seq:
-    if (f == item):
-      return True
-  return False
+    """
+    Search for item in a list
 
-"""
-This function is a utility for converting numpy arrays to the cv.cvMat format.  
-"""
+    Returns: Boolean
+    """
+    for item in seq:
+        if (f == item):
+            return True
+    return False
+
+
 def npArray2cvMat(inputMat, dataType=cv.CV_32FC1):
-  if( type(inputMat) == np.ndarray ):
-    sz = len(inputMat.shape)
-    if( sz == 1 ): #this needs to be changed so we can do row/col vectors
-      retVal = cv.CreateMat(inputMat.shape[0],1,dataType)
-      cv.SetData(retVal, inputMat.tostring(),inputMat.dtype.itemsize * inputMat.shape[0])
-    elif( sz == 2 ):
-      retVal = cv.CreateMat(inputMat.shape[0],inputMat.shape[1],dataType)
-      cv.SetData(retVal, inputMat.tostring(),inputMat.dtype.itemsize * inputMat.shape[1])
-    elif( sz > 2 ):
-      retVal = cv.CreateMat(inputMat.shape,dataType)
-      #I am going to hold off on this..... no good approach... may not be needed    
-    return retVal
-  else:
-    warnings.warn("MatrixConversionUtil: the input matrix type is not supported")
+    """
+    This function is a utility for converting numpy arrays to the cv.cvMat format.
+
+    Returns: cvMatrix
+    """
+    if( type(inputMat) == np.ndarray ):
+        sz = len(inputMat.shape)
+        if( sz == 1 ): #this needs to be changed so we can do row/col vectors
+            retVal = cv.CreateMat(inputMat.shape[0], 1, dataType)
+            cv.SetData(retVal, inputMat.tostring(), inputMat.dtype.itemsize * inputMat.shape[0])
+        elif( sz == 2 ):
+            retVal = cv.CreateMat(inputMat.shape[0], inputMat.shape[1], dataType)
+            cv.SetData(retVal, inputMat.tostring(), inputMat.dtype.itemsize * inputMat.shape[1])
+        elif( sz > 2 ):
+            retVal = cv.CreateMat(inputMat.shape, dataType)
+            #I am going to hold off on this..... no good approach... may not be needed    
+        return retVal
+    else:
+        warnings.warn("MatrixConversionUtil: the input matrix type is not supported")
 
 
