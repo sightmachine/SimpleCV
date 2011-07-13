8a5bdfdaba2b4c4f1de6cf7ea6c9dd54a7aeaa43
oostendo <nate@oostendorp.net>
2011-07-13 16:03:15 -0400

    go to 4-sp indents

--- SimpleCV/Display.py
+++ SimpleCV/Display.py
@@ -5,102 +5,106 @@ import Queue
 import json
 
 try:
-  import pygame as pg
-  pg.init()
+    import pygame as pg
+    pg.init()
   
 except ImportError:
-  raise ImportError('Error Importing Pygame/surfarray')
+    raise ImportError('Error Importing Pygame/surfarray')
 
 
 class Display:
-  """
-  WindowStream opens a window (Pygame Display Surface) to which you can write
-  images.  The default resolution is 640, 480 -- but you can also specify 0,0
-  which will maximize the display.  Flags are pygame constants, including:
+    """
+    WindowStream opens a window (Pygame Display Surface) to which you can write
+    images.  The default resolution is 640, 480 -- but you can also specify 0,0
+    which will maximize the display.  Flags are pygame constants, including:
   
-   pygame.FULLSCREEN    create a fullscreen display
-   pygame.DOUBLEBUF     recommended for HWSURFACE or OPENGL
-   pygame.HWSURFACE     hardware accelerated, only in FULLSCREEN
-   pygame.OPENGL        create an opengl renderable display
-   pygame.RESIZABLE     display window should be sizeable
-   pygame.NOFRAME       display window will have no border or controls
+    pygame.FULLSCREEN    create a fullscreen display
+    pygame.DOUBLEBUF     recommended for HWSURFACE or OPENGL
+    pygame.HWSURFACE     hardware accelerated, only in FULLSCREEN
+    pygame.OPENGL        create an opengl renderable display
+    pygame.RESIZABLE     display window should be sizeable
+    pygame.NOFRAME       display window will have no border or controls
    
-  Display should be used in a while loop with the isDone() method, which
-  checks events and sets the following internal state controls:
+    Display should be used in a while loop with the isDone() method, which
+    checks events and sets the following internal state controls:
   
-  mouseX - the x position of the mouse cursor
-  mouseY - the y position of the mouse curson
-  mouseLeft - the state of the left button
-  mouseRight - the state of the right button
-  mouseMiddle - the state of the middle button
-  mouseWheelUp - if the wheel has been clicked towards the top of the mouse
-  mouseWheelDown - if the wheel has been clicked towards the bottom of the mouse
+    mouseX - the x position of the mouse cursor
+    mouseY - the y position of the mouse curson
+    mouseLeft - the state of the left button
+    mouseRight - the state of the right button
+    mouseMiddle - the state of the middle button
+    mouseWheelUp - if the wheel has been clicked towards the top of the mouse
+    mouseWheelDown - if the wheel has been clicked towards the bottom of the mouse
  
-  """
-  screen = ''
-  eventhandler = ''
-  mq = ''
-  done = False
-  mouseX = 0
-  mouseY = 0
-  mouseLeft = 0
-  mouseMiddle = 0
-  mouseRight = 0
-  mouseWheelUp = 0
-  mouseWheelDown = 0
-  _surfarr = ""
-  
-  def __init__(self, resolution = (640, 480), flags = 0, title = "SimpleCV"):
-    self.screen = pg.display.set_mode(resolution, flags)
-    self._surfarr = pg.surfarray.array3d(self.screen)
-    if flags != pg.FULLSCREEN and flags != pg.NOFRAME:
-      pg.display.set_caption(title)
+    """
+    resolution = ''
+    screen = ''
+    eventhandler = ''
+    mq = ''
+    done = False
+    mouseX = 0
+    mouseY = 0
+    mouseLeft = 0
+    mouseMiddle = 0
+    mouseRight = 0
+    mouseWheelUp = 0
+    mouseWheelDown = 0
+    _surfarr = ""
+    
+    def __init__(self, resolution = (640, 480), flags = 0, title = "SimpleCV"):
+        self.resolution = resolution
+        self.screen = pg.display.set_mode(resolution, flags)
+        self._surfarr = pg.surfarray.array3d(self.screen)
+        if flags != pg.FULLSCREEN and flags != pg.NOFRAME:
+          pg.display.set_caption(title)
+          
+        
+    def writeFrame(self, img):
+        """
+        writeFrame copies the given Image object to the display, you can also use
+        Image.save()
+        """
+        if img.size() != self.resolution:
+            img = img.scale(self.resolution[0], self.resolution[1])
+        s = pg.image.fromstring(img.getPIL().tostring(), img.size(), "RGB")
+        self.screen.blit(s, s.get_rect())
+        pg.display.flip()
       
-    
-  def writeFrame(self, img):
-    """
-    writeFrame copies the given Image object to the display, you can also use
-    Image.save()
-    """
-    s = pg.image.fromstring(img.getPIL().tostring(), img.size(), "RGB")
-    self.screen.blit(s, s.get_rect())
-    pg.display.flip()
-    
-  def _setButtonState(self, state, button):
-    if button == 1:
-      self.mouseLeft = state
-    if button == 2:
-      self.mouseMiddle = state
-    if button == 3:
-      self.mouseRight = state
-    if button == 4:
-      self.mouseWheelUp = 1
-    if button == 5:
-      self.mouseWheelDown = 1
-    
-  def checkEvents(self):
-    """
-    checkEvents checks the pygame event queue and sets the internal display
-    values based on any new generated events
-    """
-    self.mouseWheelUp = self.mouseWheelDown = 0
-    for event in pg.event.get():
-      if event.type == pg.QUIT:
-        pg.quit()
-        self.done = True
-      if event.type == pg.MOUSEMOTION:
-        self.mouseX = event.pos[0]
-        self.mouseY = event.pos[1]
-        self.mouseLeft, self.mouseMiddle, self.mouseRight = event.buttons
-      if event.type == pg.MOUSEBUTTONUP:
-        self._setButtonState(0, event.button)
-      if event.type == pg.MOUSEBUTTONDOWN:
-        self._setButtonState(1, event.button)
+    def _setButtonState(self, state, button):
+        if button == 1:
+            self.mouseLeft = state
+        if button == 2:
+            self.mouseMiddle = state
+        if button == 3:
+            self.mouseRight = state
+        if button == 4:
+            self.mouseWheelUp = 1
+        if button == 5:
+            self.mouseWheelDown = 1
+      
+    def checkEvents(self):
+        """
+        checkEvents checks the pygame event queue and sets the internal display
+        values based on any new generated events
+        """
+        self.mouseWheelUp = self.mouseWheelDown = 0
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                self.done = True
+            if event.type == pg.MOUSEMOTION:
+                self.mouseX = event.pos[0]
+                self.mouseY = event.pos[1]
+                self.mouseLeft, self.mouseMiddle, self.mouseRight = event.buttons
+            if event.type == pg.MOUSEBUTTONUP:
+                self._setButtonState(0, event.button)
+            if event.type == pg.MOUSEBUTTONDOWN:
+                self._setButtonState(1, event.button)
 
-  def isDone(self):
-    """
-    Checks the event queue and returns True if a quit event has been issued
-    """
-    self.checkEvents()
-    return self.done
-    
+    def isDone(self):
+        """
+        Checks the event queue and returns True if a quit event has been issued
+        """
+        self.checkEvents()
+        return self.done
+      
