bbc3737999e9a4e6ae4f55788d303ab875835bcc
oostendo <nate@oostendorp.net>
2011-07-13 16:16:25 -0400

    4 space tabs

--- SimpleCV/examples/displaycam.py
+++ SimpleCV/examples/displaycam.py
@@ -4,13 +4,14 @@ import time
 from SimpleCV import *
 from SimpleCV.Display import Display, pg
 
-#create JPEG streamers
-  
-d = Display()
+d = Display(resolution = (800, 600))
 cam = Camera()
 done = False
 
+lasttime = time.time()
 
 while not d.isDone():
-  cam.getImage().flipHorizontal().save(d)
-  time.sleep(0.01)
+    cam.getImage().flipHorizontal().save(d)
+    time.sleep(0.01)
+    if d.mouseLeft:
+      d.done = True
