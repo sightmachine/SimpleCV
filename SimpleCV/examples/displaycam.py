f2adf6a899c2bb6c275a91fd20f80da4027e6d07
Anthony Oliver <anthony@ingenuitas.com>
2011-07-19 11:32:59 -0400

    Added some comments to example

--- SimpleCV/examples/displaycam.py
+++ SimpleCV/examples/displaycam.py
@@ -4,14 +4,15 @@ import time
 from SimpleCV import *
 from SimpleCV.Display import Display, pg
 
-d = Display(resolution = (800, 600))
-cam = Camera()
-done = False
+display = Display(resolution = (800, 600)) #create a new display to draw images on
+cam = Camera() #initialize the camera
+done = False # setup boolean to stop the program
 
-lasttime = time.time()
+lasttime = time.time() # add a timer for the thread to sleep
 
-while not d.isDone():
-    cam.getImage().flipHorizontal().save(d)
-    time.sleep(0.01)
-    if d.mouseLeft:
-      d.done = True
+# Loop until not needed
+while not display.isDone():
+    cam.getImage().flipHorizontal().save(display) # get image, flip it so it looks mirrored, save to display
+    time.sleep(0.01) # Let the program sleep for 1 millisecond so the computer can do other things
+    if display.mouseLeft:
+        display.done = True #if the left arrow is pressed, close the program
