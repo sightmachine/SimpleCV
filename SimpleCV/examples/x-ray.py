e0942cd7a4559cdf7225444d607cacb2217060f6
Anthony Oliver <anthony@ingenuitas.com>
2011-07-25 16:45:26 -0400

    Added x-ray example

--- SimpleCV/examples/x-ray.py
+++ SimpleCV/examples/x-ray.py
@@ -1,42 +1,36 @@
 #!/usr/bin/python 
 
-import time, sys
-from SimpleCV import Camera, Image
-import numpy as np
-import pygame
+import time
+from SimpleCV import *
+from SimpleCV.Display import Display, pg
 
-pygame.init()
+display_width = 640
+display_height = 480
+display = Display(resolution = (display_width, display_height)) #create a new display to draw images on
+cam = Camera() #initialize the camera
+done = False # setup boolean to stop the program
 
-size = width, height = 800, 600
-speed = [2, 2]
-black = 0, 0, 0
+# Loop until not needed
+while not display.isDone():
+    image = cam.getImage().flipHorizontal()
+    crop_width = 200 #set the width of the crop window
+    crop_height = 200 #set the height of the crop window
+    crop_x = display.mouseX * image.width / display_width #set the x location to scale
+    crop_y = display.mouseY * image.height / display_height #set the y location to scale
 
-screen = pygame.display.set_mode(size)
+    if(display.mouseX <= 1): #mouse outside the left of the screen
+        crop_x = 1
+    if(display.mouseY <= 1): #mouse outside the top of the screen
+        crop_y = 1
+    if(display.mouseX + crop_width >= display_width): #region outside the right side of the screen
+        crop_x = (display_width - crop_width)
+    if(display.mouseY + crop_height >= display_height): #region below the bottom of the screen
+        crop_y = (display_height - crop_height)
 
-c = Camera()
-i = c.getImage()
-arr = i.scale(int(width),int(height)).getNumpy()
-background = pygame.surfarray.make_surface(i.getNumpy())
+    cropped_image = image.crop(crop_x, crop_y, crop_width, crop_height) #crop out the section of image we want
+    xray_image = cropped_image.edges().smooth() #get the edges of cropped region
+    image.getDrawingLayer().blit(xray_image, (crop_x, crop_y)) #draw the cropped image onto the current image
 
-ball = pygame.surfarray.make_surface(arr)
-ballrect = ball.get_rect()
+    image.save(display)
+    time.sleep(0.01) # Let the program sleep for 1 millisecond so the computer can do other things
 
-while 1:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
-
-  ballrect = ballrect.move(speed)
-  if ballrect.left < 0 or ballrect.right > width:
-    speed[0] = -speed[0]
-  if ballrect.top < 0 or ballrect.bottom > height:
-    speed[1] = -speed[1]
-  
-  arr = c.getImage().edges().flipHorizontal().crop(ballrect.left, ballrect.top, int(width/8),int(height/8)).getNumpy()
-  ball = pygame.surfarray.make_surface(arr)
-  
-  screen.fill(black)
-  screen.blit(background, background.get_rect())
-  screen.blit(ball, ballrect)
-  pygame.display.flip()
