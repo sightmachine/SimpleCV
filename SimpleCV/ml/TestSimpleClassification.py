96746020c5e6c1709ac2da707ba035ad3b43881a
Katherine Scott <katherinescott@Katherine-Scotts-MacBook-Air.local>
2011-09-06 14:55:47 -0400

    nytm

--- SimpleCV/ml/TestSimpleClassification.py
+++ SimpleCV/ml/TestSimpleClassification.py
@@ -5,6 +5,8 @@ from SimpleCV.EdgeHistogramFeatureExtractor import *
 from SimpleCV.HueHistogramFeatureExtractor import *
 from SimpleCV.BOFFeatureExtractor import *
 import Orange
+import os
+import glob
 from SimpleBinaryClassifier import *
 
 w = 800
@@ -21,5 +23,19 @@ cat_test_path = "./data/cat/test/"
 cheeseburger_test_path = "./data/cheeseburger/test/"
 #classifier.train(cat_train_path,cheeseburger_train_path,disp=display,subset=50)
 classifier.load('image_data.tab')
-classifier.test(cat_test_path,cheeseburger_test_path,disp=display,subset=50)
+classifier.test(cat_test_path,cheeseburger_test_path,disp=display,subset=20)
 #bof_extractor.save("codebook.png","cbdata.txt")
+
+
+#files = glob.glob( os.path.join(cat_train_path, '*.jpg'))
+#for f in files:
+#    print(f)
+#    img = Image(f)
+#    img2 = bof_extractor.reconstruct(img)
+#    fname = f[0:-4]+'_reconstruction.png'
+#    img2.save(fname)
+#    print(fname)
+
+
+
+
