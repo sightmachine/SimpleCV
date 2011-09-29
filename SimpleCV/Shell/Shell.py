2e1445fa512b45c169679be51e2be2a287bb228f
Anthony Oliver <anthony@ingenuitas.com>
2011-09-29 15:01:18 -0400

    Got editor working from shell

--- SimpleCV/Shell/Shell.py
+++ SimpleCV/Shell/Shell.py
@@ -13,20 +13,23 @@
 # elegant programming interface for advanced users.
 #
 # more info:
-# http://sf.net/p/simplecv
+# http://www.simplecv.org
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 #load system libraries
 from SimpleCV.__init__ import *
-from subprocess import call
+
+from subprocess import call, Popen
 import platform
+import subprocess
+import time
 
 #Load simpleCV libraries
 from SimpleCV.Shell.Tutorial import *
-from SimpleCV.Shell.Help import *
 from SimpleCV.Shell.Cheatsheet import *
 from SimpleCV.Shell.Example import *
+
 
 #libraries for the shell
 from IPython.Shell import IPShellEmbed
@@ -41,13 +44,39 @@ def clear():
 def magic_clear(self, arg):
   clear()
 
+
+def magic_editor(self, arg):
+
+    os_type = platform.system().lower()
+    print "please wait while checking for editor updates..."
+    time.sleep(2)
+    if os_type == "windows":
+        cmd = "cmd /c git submodule update --init --recursive"
+        call(["cmd","/c","git","submodule","update","--init","--recursive"])
+        path = "./SimpleCV/utils/cloud9/bin/cloud9-win32.bat"
+        
+    else:
+        cmd = "git submodule update --init --recursive"
+        call(["git","submodule","update","--init","--recursive"])
+        path = "./SimpleCV/utils/cloud9/bin/cloud9.sh"
+
+
+    
+    #~ call(cmd) # update the editor
+
+    print "...checking for updates complete"
+    print "launching the editor"
+    flags = "-w"
+    args = "../../"
+    Popen([path, flags, args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
 """
 If you run SimpleCV directly, it will launch an ipython shell
 """
 def main():
     clear()
     banner = '+----------------------------------------------------+\n'
-    banner += ' SimpleCV [interactive shell]\n'
+    banner += ' SimpleCV [interactive shell] - http://simplecv.org\n'
     banner += '+----------------------------------------------------+\n'
     banner += '\n'
     banner += 'Commands: \n'
@@ -55,27 +84,18 @@ def main():
     banner += '\t"clear" to clear the shell screen\n'
     banner += '\t"tutorial" to begin the SimpleCV interactive tutorial\n'
     banner += '\t"cheatsheet" gives a cheatsheet of all the shell functions\n' 
-    banner += '\t"simplehelp" gives list of commands or help on a certain command\n'
     banner += '\t"example" gives a list of examples you can run'
     banner += '\n'
     banner += 'Usage:\n'
     banner += '\tdot complete works to show library\n'
     banner += '\tfor example: Image().save("/tmp/test.jpg") will dot complete\n'
     banner += '\tjust by touching TAB after typing Image().\n'
-    banner += '\n'
     banner += 'API Documentation:\n'
     banner += '\t"help function_name" will give in depth documentation of API\n'
-    banner += '\t\texample:'
-    banner += 'help Image or ?Image\n'
-    banner += '\t\twill give the in-depth information about that class\n'
-    banner += '\t"?function_name" will give the quick API documentation\n'
-    banner += '\t\texample:'
-    banner += '?Image.save\n'
-    banner += '\t\twill give help on the image save function'
-
-    
-
-    
+    banner += '\texample: help Image\n'
+    banner += 'Editor:\n'
+    banner += '\t"editor" will run the SimpleCV code editor in a browser\n'
+   
     exit_msg = '\nExiting the SimpleCV interactive shell\n'
     
 
@@ -87,9 +107,9 @@ def main():
     scvShell.set_exit_msg(exit_msg)
     scvShell.IP.api.expose_magic("tutorial",magic_tutorial)
     scvShell.IP.api.expose_magic("clear", magic_clear)
-    scvShell.IP.api.expose_magic("simplehelp", magic_help)
     scvShell.IP.api.expose_magic("cheatsheet", magic_cheatsheet)
     scvShell.IP.api.expose_magic("example", magic_examples)
+    scvShell.IP.api.expose_magic("editor", magic_editor)
     
 
     #Note that all loaded libraries are inherited in the embedded ipython shell
