dabab39a9e954a09520cffbd434efac5bb1192fe
Katherine Scott <katherineAScott@gmail.com>
2011-10-31 12:15:36 -0400

    grrrr, getting imports right

--- SimpleCV/__init__.py
+++ SimpleCV/__init__.py
@@ -11,17 +11,22 @@ from SimpleCV.Font import *
 from SimpleCV.ColorModel import *
 from SimpleCV.DrawingLayer import *
 from SimpleCV.BlobMaker import *
-from SimpleCV.SegmentationBase import *
-from SimpleCV.ColorSegmentation import *
-from SimpleCV.DiffSegmentation import *
-from SimpleCV.BOFFeatureExtractor import *
-from SimpleCV.RunningSegmentation import *
-from SimpleCV.CodebookSegmentation import *
-from SimpleCV.FeatureExtractorBase import *
-from SimpleCV.HueHistogramFeatureExtractor import *
-from SimpleCV.EdgeHistogramFeatureExtractor import *
-from SimpleCV.HaarLikeFeatureExtractor import *
-from SimpleCV.HaarLikeFeature import *
+from SimpleCV.Segmentation.SegmentationBase import *
+#from SimpleCV.Segmentation.ColorSegmentation import *
+#from SimpleCV.Segmentation.DiffSegmentation import *
+#from SimpleCV.Segmentation.RunningSegmentation import *
+#from SimpleCV.analysis.BOFFeatureExtractor import *
+#from SimpleCV.analysis.CodebookSegmentation import *
+#from SimpleCV.analysis.FeatureExtractorBase import *
+#from SimpleCV.analysis.HueHistogramFeatureExtractor import *
+#from SimpleCV.analysis.EdgeHistogramFeatureExtractor import *
+#from SimpleCV.analysis.HaarLikeFeatureExtractor import *
+#from SimpleCV.analysis.HaarLikeFeature import *
+#from SimpleCV.ml.BinarySVMClassifier import *
+#from SimpleCV.ml.TreeClassifier import *
+#from SimpleCV.ml.KNNClassifier import *
+#from SimpleCV.ml.NaiveBayesClassifier import *
+
 if (__name__ == '__main__'):
     from SimpleCV.Shell import *
     main()
