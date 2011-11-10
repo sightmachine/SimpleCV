9f45042d24172320f797ba1782ed2b0d097f50f4
Anthony Oliver <email@xamox.net>
2011-11-10 00:36:20 -0500

    Updated with JSON support, download support, and update of console on UI working

--- install/simplecv-installer.py
+++ install/simplecv-installer.py
@@ -5,8 +5,17 @@ import urllib2
 import warnings
 import zipfile
 import Tkinter
+import simplejson as json
 
+
+#Global Definitions
 output_status = ""
+cache_directory = ""
+repo_url = ""
+version_to_use = "1.2"
+operating_system = None
+app = None
+current_file = None
 
 def SaveButtonClick():
     run()
@@ -15,109 +24,149 @@ def CancelButtonClick():
     sys.exit()
 
 def writeText(text):
-    output_status.insert(Tkinter.END, text)
-    output_status.insert(Tkinter.END, "\n")
+	global output_status
+	
+	output_status.insert(Tkinter.END, text)
+	output_status.insert(Tkinter.END, "\n")
+	sys.stdout.write(text)
+	sys.stdout.write("\n")
 
 def download(URL):
-    if URL == None:
-        warnings.warn("Please provide URL")
-        return None
+	global cache_directory
+	global operating_system
+	global current_file
+	
+	if URL == None:
+		warnings.warn("Please provide URL")
+		return None
 
-    tmpdir = tempfile.mkdtemp()
-    filename = os.path.basename(URL)
-    path = tmpdir + "\\" + filename
-    data = urllib2.urlopen(URL)
+	tmpdir = cache_directory
+	writeText("downloading")
+	writeText(URL)
+	filename = os.path.basename(URL)
+	current_file = filename
+	filepath = tmpdir + "\\" + filename
+	if operating_system == "windows":
+		path = tmpdir + "\\" + filename
+	else:
+		path = tmpdir + "/" + filename
+		
+	data = urllib2.urlopen(URL)
+	chunk_read(data, report_hook=chunk_report)
+	writeText("The file path is:")
+	writeText(filepath)
+	writeText("Saving file to disk please wait....")
+	with open(filepath, "wb") as local_file:
+		local_file.write(data.read())
 
-    print("Saving file to disk please wait....")
-    writeText("Saving file to disk please wait....")
-    with open(path, "wb") as local_file:
-        local_file.write(data.read())
-
-    print filename + " saved to disk"
-    return (path, filename)
+	writeText(filename + " saved to disk")
+	return (filepath, filename)
 
 
-def download_and_extract(URL):
-    if URL == None:
-        warnings.warn("Please provide URL")
-        return None
+def extract(path):
+	tmpdir = cache_directory
+	zfile = zipfile.ZipFile(path)
+	writeText("Extracting zipfile")
+	try:
+		zfile.extractall(tmpdir)
+	except:
+		warnings.warn("Couldn't extract zip file")
+		return None
 
-    tmpdir = tempfile.mkdtemp()
-    filename = os.path.basename(URL)
-    path = tmpdir + "/" + filename
-    zdata = urllib2.urlopen(URL)
+	return tmpdir
 
-    writeText("Saving file to disk please wait....")
-    with open(path, "wb") as local_file:
-        local_file.write(zdata.read())
+def chunk_report(bytes_so_far, chunk_size, total_size):
+	global app
+	global output_status
+	global current_file
+	
+	percent = float(bytes_so_far) / total_size
+	percent = round(percent*100, 2)
+	writeText("Downloaded percent: " + str(percent) + "% of file: " + current_file)
 
-    zfile = zipfile.ZipFile(path)    
-    writeText("Extracting zipfile")
-    try:
-        zfile.extractall(tmpdir)
-    except:
-        warnings.warn("Couldn't extract zip file")
-        return None
+	if bytes_so_far >= total_size:
+		sys.stdout.write('\n')
 
-    return tmpdirw
+	app.update()
+	output_status.yview(Tkinter.END)
+	
+	
+def chunk_read(response, chunk_size=8192, report_hook=None):
+	total_size = response.info().getheader('Content-Length').strip()
+	total_size = int(total_size)
+	bytes_so_far = 0
 
+	while 1:
+		chunk = response.read(chunk_size)
+		bytes_so_far += len(chunk)
+
+		if not chunk:
+			break
+
+		if report_hook:
+			report_hook(bytes_so_far, chunk_size, total_size)
+
+	return bytes_so_far
 
 def run_install(path, name = "UNKNOWN"):
-    print("Running install for:" + name)
-    print("on path: " + path)
     writeText("Running install for:" + name)
     writeText("on path: " + path)
     os.system(path)
 
 
 def run():
-    print "Running the installer"
-    filelist = [
-        "http://www.python.org/ftp/python/2.7.2/python-2.7.2.msi",
-        "http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe",
-        "http://downloads.sourceforge.net/project/scipy/scipy/0.10.0rc1/scipy-0.10.0rc1-win32-superpack-python2.7.exe",
-        "http://downloads.sourceforge.net/project/numpy/NumPy/1.6.1/numpy-1.6.1-win32-superpack-python2.7.exe",
-        "http://archive.ipython.org/release/0.11/ipython-0.11.win32-setup.exe",
-        "http://downloads.sourceforge.net/project/opencvlibrary/opencv-win/2.3.1/OpenCV-2.3.1-win-superpack.exe"
-        ]
+	writeText("Running the installer")
+	cache_directory = tempfile.mkdtemp()
+	filelist = json.load(open("manifest.json", "r"))
+	urllist = filelist[version_to_use][operating_system]
 
-    for file in filelist:
-        print "Running"
-        print file
-		writeText("Running")
-		writeText(file)
-		path,name = download(file)
+	for url in urllist:
+		path,name = download(url)
 		run_install(path, name)
 
 
+
 if __name__ == "__main__":
-    app = Tkinter.Tk()
 
-    header = "Welcome to the SimpleCV installer"
-    instructions = "This installer will try to download and install all the libraries you need"
-    instructions_cont = "please be patient while files download, if you are having problems visit:"
-    website = "http://www.simplecv.org"
-    
-    
-    w = Tkinter.Label(app, text=header, font=("default",20))
-    w.pack(padx=30, pady=20)
-    w = Tkinter.Label(app, text="")
-    w.pack()
-    w = Tkinter.Label(app, text=instructions)
-    w.pack()
-    w = Tkinter.Label(app, text=instructions_cont)
-    w.pack()
-    w = Tkinter.Label(app, text=website)
-    w.pack(pady=10)
-    w = Tkinter.Button(app, text="Install SimpleCV", command=SaveButtonClick)
-    w.pack(side=Tkinter.RIGHT, padx=10, pady=10)
-    w = Tkinter.Button(app, text="Cancel", command=CancelButtonClick)
-    w.pack(side=Tkinter.RIGHT, padx=10, pady=10)
+	if(len(sys.argv) > 1):
+		repo_url = sys.argv[1]
+	if(len(sys.argv) > 2):
+		version_to_use = sys.argv[2]
 
-    output_status=Tkinter.Text(app,height=10,width=50,background='black', foreground='white')
-    output_status.pack(side=Tkinter.BOTTOM)
-    output_status.insert(Tkinter.END, "Output Console\n")
-    output_status.insert(Tkinter.END, "------------------------------\n")
+	if(sys.platform.lower() == "linux2"):
+		operating_system = "linux"
+	else:
+		operating_system = sys.platform.lower()
+		
+	header = "Welcome to the SimpleCV installer"
+	instructions = "This installer will try to download and install all the libraries you need"
+	instructions_cont = "please be patient while files download, if you are having problems visit:"
+	website = "http://www.simplecv.org"
 
-    app.title('SimpleCV Installer - http://www.simplecv.org')
-    app.mainloop()
+	app = Tkinter.Tk()
+	w = Tkinter.Label(app, text=header, font=("default",20))
+	w.pack(padx=30, pady=20)
+	w = Tkinter.Label(app, text="")
+	w.pack()
+	w = Tkinter.Label(app, text=instructions)
+	w.pack()
+	w = Tkinter.Label(app, text=instructions_cont)
+	w.pack()
+	w = Tkinter.Label(app, text=website)
+	w.pack(pady=10)
+	w = Tkinter.Button(app, text="Install SimpleCV", command=SaveButtonClick)
+	w.pack(side=Tkinter.RIGHT, padx=10, pady=10)
+	w = Tkinter.Button(app, text="Cancel", command=CancelButtonClick)
+	w.pack(side=Tkinter.RIGHT, padx=10, pady=10)
+
+	scrollbar = Tkinter.Scrollbar(app)
+	scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+	
+	output_status=Tkinter.Text(app,height=10,width=50,background='black', foreground='white', yscrollcommand=scrollbar.set)
+	output_status.pack(side=Tkinter.BOTTOM)
+	scrollbar.config(command=output_status.yview)
+	output_status.insert(Tkinter.END, "Output Console\n")
+	output_status.insert(Tkinter.END, "------------------------------\n")
+
+	app.title('SimpleCV Installer - http://www.simplecv.org')
+	app.mainloop()
