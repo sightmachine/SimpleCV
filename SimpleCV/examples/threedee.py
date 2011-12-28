2851159a86e95bbeadb2f07c78e88b0f604bb68c
Katherine Scott <katherineAScott@gmail.com>
2011-12-28 02:29:32 -0500

    fixed it

--- SimpleCV/examples/threedee.py
+++ SimpleCV/examples/threedee.py
@@ -4,13 +4,15 @@ import sys
 import os
 from SimpleCV import *
 
-offset = (60,0)
+offset = (0,0)
 left = Image("left.png")
 right = Image("right.png")
 (r,g,b)=left.splitChannels()
-left_blue = left.mergeChannels(None,g,b);
+left_blue = left.mergeChannels(None,b,g);
+left_blue.save("blue.png")
 (r,g,b) = right.splitChannels()
-right_red = right.mergeChannels(r,g,None);
+right_red = right.mergeChannels(r,None,None);
+right_red.save("red.png")
 sz = (left.width+offset[0],left.height+offset[1])
 output = left_blue.embiggen(size=sz,pos=(0,0))
 output = output.blit(right_red,alpha=0.5,pos=offset)
