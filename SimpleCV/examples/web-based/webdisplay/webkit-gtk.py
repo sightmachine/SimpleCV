7e685a994a19dfb0fbdcdae23b3ad31c97c79e3f
Anthony Oliver <anthony@ingenuitas.com>
2012-02-23 23:51:03 -0500

    Added a bunch more stuff, have basic web demo with flask working.  Need to get features rendering next

---
+++ SimpleCV/examples/web-based/webdisplay/webkit-gtk.py
@@ -0,0 +1,15 @@
+import gtk 
+import webkit 
+
+view = webkit.WebView() 
+
+sw = gtk.ScrolledWindow() 
+sw.add(view) 
+
+win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+win.resize(800,600)
+win.add(sw) 
+win.show_all() 
+
+view.open("http://localhost:5000/") 
+gtk.main()
