71e79386140a83b483ce746e8d7f317cc77e5bdb
Anthony Oliver <anthony@ingenuitas.com>
2012-03-19 12:57:20 -0400

    Trying to fix sample files install windows

--- setup.py
+++ setup.py
@@ -6,58 +6,58 @@ import os, sys, glob, fnmatch
 
 
 ## Added 10 Jan 2008
-from distutils.core import setup, Extension
-import distutils.command.install_data
-
-## Code borrowed from wxPython's setup and config files
-## Thanks to Robin Dunn for the suggestion.
-## I am not 100% sure what's going on, but it works!
-def opj(*args):
-    path = os.path.join(*args)
-    return os.path.normpath(path)
-
-## Added 10 Jan 2008
-# Specializations of some distutils command classes
-class wx_smart_install_data(distutils.command.install_data.install_data):
-    """need to change self.install_dir to the actual library dir"""
-    def run(self):
-        install_cmd = self.get_finalized_command('install')
-        self.install_dir = getattr(install_cmd, 'install_lib')
-        return distutils.command.install_data.install_data.run(self)
-
-def find_data_files(srcdir, *wildcards, **kw):
-    # get a list of all files under the srcdir matching wildcards,
-    # returned in a format to be used for install_data
-    def walk_helper(arg, dirname, files):
-        if '.svn' in dirname:
-            return
-        names = []
-        lst, wildcards = arg
-        for wc in wildcards:
-            wc_name = opj(dirname, wc)
-            for f in files:
-                filename = opj(dirname, f)
-
-                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
-                    names.append(filename)
-        if names:
-            lst.append( (dirname, names ) )
-
-    file_list = []
-    recursive = kw.get('recursive', True)
-    if recursive:
-        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
-    else:
-        walk_helper((file_list, wildcards),
-                    srcdir,
-                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
-    return file_list
-
-## This is a list of files to install, and where:
-## Make sure the MANIFEST.in file points to all the right 
-## directories too.
-files = find_data_files('SimpleCV/', '*.*')
-
+#~ from distutils.core import setup, Extension
+#~ import distutils.command.install_data
+#~ 
+#~ ## Code borrowed from wxPython's setup and config files
+#~ ## Thanks to Robin Dunn for the suggestion.
+#~ ## I am not 100% sure what's going on, but it works!
+#~ def opj(*args):
+    #~ path = os.path.join(*args)
+    #~ return os.path.normpath(path)
+#~ 
+#~ ## Added 10 Jan 2008
+#~ # Specializations of some distutils command classes
+#~ class wx_smart_install_data(distutils.command.install_data.install_data):
+    #~ """need to change self.install_dir to the actual library dir"""
+    #~ def run(self):
+        #~ install_cmd = self.get_finalized_command('install')
+        #~ self.install_dir = getattr(install_cmd, 'install_lib')
+        #~ return distutils.command.install_data.install_data.run(self)
+#~ 
+#~ def find_data_files(srcdir, *wildcards, **kw):
+    #~ # get a list of all files under the srcdir matching wildcards,
+    #~ # returned in a format to be used for install_data
+    #~ def walk_helper(arg, dirname, files):
+        #~ if '.svn' in dirname:
+            #~ return
+        #~ names = []
+        #~ lst, wildcards = arg
+        #~ for wc in wildcards:
+            #~ wc_name = opj(dirname, wc)
+            #~ for f in files:
+                #~ filename = opj(dirname, f)
+#~ 
+                #~ if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    #~ names.append(filename)
+        #~ if names:
+            #~ lst.append( (dirname, names ) )
+#~ 
+    #~ file_list = []
+    #~ recursive = kw.get('recursive', True)
+    #~ if recursive:
+        #~ os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+    #~ else:
+        #~ walk_helper((file_list, wildcards),
+                    #~ srcdir,
+                    #~ [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    #~ return file_list
+#~ 
+#~ ## This is a list of files to install, and where:
+#~ ## Make sure the MANIFEST.in file points to all the right 
+#~ ## directories too.
+#~ files = find_data_files('SimpleCV/', '*.*')
+#~ 
 
 
 
@@ -91,5 +91,8 @@ setup(name="SimpleCV",
   zip_safe = True,
   requires=['cv', 'numpy', 'scipy', 'pygame', 'pil'],
   scripts=['scripts/simplecv'],
-  data_files = files,
+  package_data  = {
+            'SimpleCV': ['sampleimages/*']
+  }
+  #~ data_files = files,
   )
