3b834c532f4d2c8ce3f8644bac530f17c9ac6b7c
Vijay Mahantesh SM <vijaym123@gmail.com>
2012-04-20 23:11:35 +0530

    Issue #76 resolved. Gives system information and version information about 1) Operating System 2) PIL version 3) OpenCV version 4) Orange Version

--- SimpleCV/__init__.py
+++ SimpleCV/__init__.py
@@ -17,3 +17,34 @@ from SimpleCV.MachineLearning import *
 if (__name__ == '__main__'):
     from SimpleCV.Shell import *
     main(sys.argv)
+    
+def system():
+    """
+    **SUMMARY**
+    Output of this function includes various informations related to the system and versions of libraries. 
+    Main purpose is during :
+       1) Submiting bug reports 
+       2) Checking the version and later upgrading the library versions based on the current output. 
+   
+    **RETURNS**
+    None  
+    
+    **EXAMPLE**
+    >>> import SimpleCV
+    >>> SimpleCV.system()
+    """         		
+    if (os.name == 'posix'):
+    	print "Operating system : Linux"
+    elif (os.name == 'nt'):
+    	print "Operating system : Windows"
+    elif (os.name == 'mac'):
+    	print "Operating system : Mac"
+    print "PIL version : " + pil.VERSION
+    try :
+    	from cv2 import __version__
+    	print "Open CV version : " + __version__
+    except :
+    	print "Open CV version : " + "2.1"    
+    print "Orange Version : " + orange.version
+    print "PyGame Version : " + pg.__version__
+    print "Pickle Version : " + pickle.__version__
