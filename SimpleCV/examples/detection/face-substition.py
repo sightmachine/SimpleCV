c01a3298b83c848170ffba07998dbd89cd41e6c9
Katherine Scott <katherineAScott@gmail.com>
2012-04-26 16:37:09 -0400

    messing with an example for giggles

--- SimpleCV/examples/detection/face-substition.py
+++ SimpleCV/examples/detection/face-substition.py
@@ -8,7 +8,7 @@ from SimpleCV import *
 
 cam = Camera() #initialize the camera
 substition_image = "../../sampleimages/troll_face.png"
-
+troll_face = Image(substition_image)
 # Loop forever
 while True:
     image = cam.getImage().flipHorizontal().scale(320, 240) # get image, flip it so it looks mirrored, scale to speed things up
@@ -16,8 +16,9 @@ while True:
     #if there were faces found do something
     if faces:
         face = faces[-1]
-        troll_face = Image(substition_image).scale(face.height(), face.width()) #load the image to super impose and scale it correctly
-        image.dl().blit(troll_face, (face.points[0][0], face.points[0][1])) #super impose the new face on the existing face
+        troll = troll_face.scale(face.height(), face.width()) #load the image to super impose and scale it correctly
+        mymask = troll.invert()
+        image = image.blit(troll, face.topLeftCorner(),alphaMask=mymask) #super impose the new face on the existing face
     image.show() #display the image
     time.sleep(0.01) # Let the program sleep for 1 millisecond so the computer can do other things
 
