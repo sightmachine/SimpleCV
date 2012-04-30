8771b835255e076c3938c66de057290b0bd7ff23
Vijay Mahantesh SM <vijaym123@gmail.com>
2012-04-30 21:21:51 +0530

    Update SimpleCV/__init__.py

--- SimpleCV/__init__.py
+++ SimpleCV/__init__.py
@@ -17,7 +17,6 @@ from SimpleCV.MachineLearning import *
 if (__name__ == '__main__'):
     from SimpleCV.Shell import *
     main(sys.argv)
-    
 def system():
     """
     **SUMMARY**
@@ -32,19 +31,23 @@ def system():
     **EXAMPLE**
     >>> import SimpleCV
     >>> SimpleCV.system()
-    """         		
-    if (os.name == 'posix'):
-    	print "Operating system : Linux"
-    elif (os.name == 'nt'):
-    	print "Operating system : Windows"
-    elif (os.name == 'mac'):
-    	print "Operating system : Mac"
-    print "PIL version : " + pil.VERSION
-    try :
-    	from cv2 import __version__
-    	print "Open CV version : " + __version__
-    except :
-    	print "Open CV version : " + "2.1"    
-    print "Orange Version : " + orange.version
-    print "PyGame Version : " + pg.__version__
-    print "Pickle Version : " + pickle.__version__
+    """         	
+    try:
+        import platform
+        print "System : ", platform.system()
+        print "OS version : ", platform.version()
+        print "Python version :", platform.python_version()
+        try :
+            from cv2 import __version__
+            print "Open CV version : " + __version__
+        except :
+            print "Open CV version : " + "2.1"
+        print "PIL version : ", pil.VERSION
+        print "Orange Version : " + orange.version
+        print "PyGame Version : " + pg.__version__
+        print "Pickle Version : " + pickle.__version__
+    except:
+        print "You need to install Platform to use this function"
+        print "to install you can use:"
+        print "easy_install platform"
+    return
