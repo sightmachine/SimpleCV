a262e9d89f56e1beff2a136266f96bcc9e8e3eaa
Katherine Scott <katherineAScott@gmail.com>
2012-05-02 15:29:16 -0400

    made face tracker hide faces

--- SimpleCV/examples/detection/facetrack.py
+++ SimpleCV/examples/detection/facetrack.py
@@ -4,10 +4,13 @@ from SimpleCV import *
 
 cam = Camera() #initialize the camera
 
+hc = HaarCascade("../../Features/HaarCascades/face.xml")
 # Loop forever
 while True:
     image = cam.getImage().flipHorizontal().scale(320, 240) # get image, flip it so it looks mirrored, scale to speed things up
-    faces = image.findHaarFeatures("facetrack-training.xml") # load in trained face file
-    if faces: faces.draw()
+    faces = image.findHaarFeatures(hc) # load in trained face file
+    if faces:
+        bb = faces[-1].boundingBox()
+        image = image.pixelize(10,region=(bb[0],bb[1],bb[2],bb[3]))
     image.show() #display the image
     time.sleep(0.01) # Let the program sleep for 1 millisecond so the computer can do other things
