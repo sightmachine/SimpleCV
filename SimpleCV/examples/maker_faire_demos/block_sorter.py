dffdf784087753e4ebd5e0ed456905cbda68db8e
Katherine Scott <katherineAScott@gmail.com>
2012-05-15 12:59:13 -0400

    okay the core of this works - now for the gamifcation

--- SimpleCV/examples/maker_faire_demos/block_sorter.py
+++ SimpleCV/examples/maker_faire_demos/block_sorter.py
@@ -38,6 +38,7 @@ def estimateColorMatrix(t, shape=(4,4), palette = [Color.RED, Color.LEGO_ORANGE,
             retVal[i,j] = palette[best[1]]
     return retVal.rotate90().flipHorizontal() 
 
+
 def createOutputImage(colors,template,shape=(4,4)):
     w = template.width
     h = template.height
@@ -52,10 +53,41 @@ def createOutputImage(colors,template,shape=(4,4)):
     retVal = retVal.blit(template,alphaMask = template.invert() )
     return retVal
     
-    
+def userImageIsCorrect( userImage, truthImage ):
+    img00 = userImage - truthImage
+    img90 = userImage.rotate90().flipVertical() - truthImage
+    img180 = userImage.flipVertical().flipHorizontal() - truthImage
+    img270 = userImage.rotate90().flipHorizontal() - truthImage
+    r00 = np.sum(img00.getNumpy())
+    r90 = np.sum(img90.getNumpy())
+    r180 = np.sum(img180.getNumpy())
+    r270 = np.sum(img270.getNumpy())
+    threshold = 0.50
+    print [r00,r90,r180,r270]
+    retVal = False
+    if( r00 < threshold or r90 < threshold or r180 < threshold or r270 < threshold):
+        retVal = True
+    return retVal
+
+def renderResult( userImg, isCorrect, correct, correctMask, incorrect, incorrectMask):
+    if(isCorrect):
+        result = userImg.blit(correct,pos=(150,150),mask=correctMask)
+    else:
+        result = userImg.blit(incorrect,pos=(150,150),mask=incorrectMask)
+    return result
+
 maskh = 600
 tempimg = tempimg.resize(maskh,maskh)
 template = Image("template.png")
+
+
+redx = Image("redx.png")
+redxm = redx.binarize().invert()
+greencheck = Image("greencheck.png")
+greencheckm = greencheck.binarize()
+
+gametemplates = ImageSet("./truthdata/")
+
 while disp.isNotDone():
     t = cam.getImage().scale(scale_f)
     mask = t.colorDistance(Color.WHITE).binarize().invert().dilate(4)
@@ -68,11 +100,13 @@ while disp.isNotDone():
             result = estimateColorMatrix(cm)
             model = createOutputImage(result,template)
             #model = model.rotate(-90)
-            output = t
+            #output = t
+            good = userImageIsCorrect(result,gametemplates[0])
+            output = renderResult( t, good, greencheck,greencheckm,redx,redxm) 
             #result = result.resize(50,50)
             #output = t.blit(result)
             #output = output.blit(cm.resize(50,50),(0,51))
-            #output = output.blit(result2.resize(50,50),(0,102))
+            #outut = output.blit(result2.resize(50,50),(0,102))
             
             output = model.sideBySide(output)
             blobs[-1].draw(width=3)
