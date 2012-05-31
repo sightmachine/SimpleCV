856866616d7fe44836c9d5196dd402d8df6b15b9
Nathan Oostendorp <nate@oostendorp.net>
2012-05-31 11:16:16 -0400

    return haarfeature getstate

--- SimpleCV/Features/Detection.py
+++ SimpleCV/Features/Detection.py
@@ -467,7 +467,7 @@ class HaarFeature(Feature):
     def __getstate__(self):
         dict = self.__dict__.copy()
         del dict["classifier"]
-	return dict
+        return dict
               
       
     def meanColor(self):
