5a54aa7360af0c8d856995cd378f1e623a081c5d
Anthony Oliver <email@xamox.net>
2012-07-18 11:59:14 -0400

    Removed unicode unit test as it's not working

--- SimpleCV/tests/tests.py
+++ SimpleCV/tests/tests.py
@@ -2776,10 +2776,6 @@ def test_findKeypoints_all():
      perform_diff(results,name_stem,tolerance=8)    
   pass
 
-def test_unicode_imageload():
-    i = Image(unicode(barcode)) # unicode vs string is irrelevant
-    prod = lambda li: reduce(operator.mul, li)
-    assert prod(i.size()) > 0, "unicode image loading is broken, got image with zero area"
     
 def test_upload_flickr():
     try:
