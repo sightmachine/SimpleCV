fb93aa280c9775ab35aa06a02a07ebc52ad31a7b
Anthony Oliver <email@xamox.net>
2012-07-24 11:13:34 -0400

    Moved test to another folder

---
+++ SimpleCV/tests/optional/test_haar_cascade.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from SimpleCV import * 
+
+FACECASCADE = 'face.xml'
+
+testimage = "sampleimages/orson_welles.jpg"
+testoutput = "sampleimages/orson_welles_face.jpg"
+
+testneighbor_in = "sampleimages/04000.jpg"
+testneighbor_out = "sampleimages/04000_face.jpg"
+
+def test_haarcascade():
+  img = Image(testimage)
+  faces = img.findHaarFeatures(FACECASCADE)
+
+  if (faces):
+    faces.draw()
+    img.save(testoutput)
+  else: 
+    assert False
+
+def test_minneighbors(img_in=testneighbor_in, img_out=testneighbor_out):
+    img = Image(img_in)
+    faces = img.findHaarFeatures(FACECASCADE, min_neighbors=20)
+    if faces:
+        faces.draw()
+        img.save(img_out)
+    # if len(faces) > 1
+    assert len(faces) <= 1, "Haar Cascade is potentially ignoring the 'HIGH' min_neighbors of 20"
