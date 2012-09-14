15fe84daafd557f9b6eb0a0707a815b883a92511
kscottz <kat@sightmachine.com>
2012-09-14 14:38:30 -0400

    now to fill in the blanks with image processing

--- SimpleCV/Features/PlayingCards/TestScript.py
+++ SimpleCV/Features/PlayingCards/TestScript.py
@@ -4,8 +4,23 @@ from PlayingCardFactory import *
 pcf = PlayingCardFactory()
 data,labels = GetFullDataSet()
 print len(data)
-for d in data:
-    fs = pcf.process(d)
-    fs.show()
-#   #    time.sleep(0.1)
+datapoints = zip(data,labels)
+datapoints = datapoints[0:100]
+result = []
+passing = 0
+for d in datapoints:
+    img = d[0]
+    label = d[1]
+    fs = pcf.process(img)
+    r = (None,None)
+    if( fs is not None ):
+        fs.show()
+        r = fs[0].getCard()
+    result.append(r)
+    if(r==label):
+        passing += 1
+        print "PASS: "+str(label)+"->"+str(r)
+    else:
+        print "FAIL: "+str(label)+"->"+str(r)
+    time.sleep(0.001)
 #print labels
