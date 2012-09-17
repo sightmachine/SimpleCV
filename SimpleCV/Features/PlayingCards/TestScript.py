47f0b8cbd5efd4822439b6f89d3c90cb9fe02f69
Katherine Scott <katherineAScott@gmail.com>
2012-09-17 11:37:49 -0400

    okay we are segmenting out blobs

--- SimpleCV/Features/PlayingCards/TestScript.py
+++ SimpleCV/Features/PlayingCards/TestScript.py
@@ -34,39 +34,55 @@ pcf = PlayingCardFactory()
 data,labels = GetFullDataSet()
 print len(data)
 datapoints = zip(data,labels)
-datapoints = datapoints[0:200]
+#datapoints = datapoints[0:400]
 result = []
 passing = 0
+color = Color()
 for d in datapoints:
     img = d[0]
     label = d[1]
 #    img = img.crop(img.width/3,0,2*img.width/3,img.height)
     #    img = img.equalize()
-    img = img.edges()
-    l = img.findLines(threshold=10)
-    if( l is not None ):
-        v = 70
-        h = 30
-        vl = l.filter(np.abs(l.angle()) > v)
-        vl = vl.filter(vl.length() > img.height/6)
-        hl = l.filter(np.abs(l.angle()) < h)
-        hl = hl.filter(hl.length() > img.width/8)
-        vl.draw(color=Color.RED,width=3)
-        hl.draw(color=Color.BLUE,width=3)
-        derp = GetParallelSets(vl)
-        color = Color()
-        for d in derp:
-#            img.clearLayers()
-        #     l.draw()
-            c = color.getRandom()
-            d[0].draw(color=c,width=3)
-            d[1].draw(color=c,width=3)
-            img.show()
+    ts = 0.01
+    e = img.edges(t1=1,t2=50)#.dilate(2).erode(2)
+    e2 = img.edges(t1=10,t2=100)
+    e3 = img.sobel()
+    e = e+e2+e3
+    e = e.dilate(2).erode(1)
+    final = img-e
+    bin= final.threshold(150).morphClose()
+    max_sz = img.width*img.height
+    b = img.findBlobsFromMask(bin,minsize=max_sz*0.005,maxsize=max_sz*0.3)
+    b = b.sortDistance(point=(img.width/2,img.height/2))
+    if( b is not None ):
+        b[0].draw(color=color.getRandom(),width=-1)
+    img.show()
+    time.sleep(.1)
+#    img = img.edges()
+ #    l = img.findLines(threshold=10)
+#     if( l is not None ):
+#         v = 70
+#         h = 30
+#         vl = l.filter(np.abs(l.angle()) > v)
+#         vl = vl.filter(vl.length() > img.height/6)
+#         hl = l.filter(np.abs(l.angle()) < h)
+#         hl = hl.filter(hl.length() > img.width/8)
+#         vl.draw(color=Color.RED,width=3)
+#         hl.draw(color=Color.BLUE,width=3)
+#         derp = GetParallelSets(vl)
+#         color = Color()
+#         for d in derp:
+# #            img.clearLayers()
+#         #     l.draw()
+#             c = color.getRandom()
+#             d[0].draw(color=c,width=3)
+#             d[1].draw(color=c,width=3)
+#             img.show()
 
-        #top = np.min([8,len(l)])
-        #l[-1*top:-1].draw(color=Color.RED,width=3)
-        img.show()
-        time.sleep(.5)
+#         #top = np.min([8,len(l)])
+#         #l[-1*top:-1].draw(color=Color.RED,width=3)
+#         img.show()
+#         time.sleep(.5)
         #l.show(color=Color.RED,width=3)
     
     # fs = pcf.process(img)
