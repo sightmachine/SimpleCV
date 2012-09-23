9550a75ae204c6cb53974888a607c3f2fc7e5aaa
kscottz <kat@sightmachine.com>
2012-09-23 16:01:27 -0400

    okay this runs and works... with my camera ... I doubt with others

--- SimpleCV/Features/PlayingCards/IdentifyCards.py
+++ SimpleCV/Features/PlayingCards/IdentifyCards.py
@@ -7,6 +7,7 @@ import numpy as np
 
 cam = Camera()
 pcf = PlayingCardFactory()
+count = 0
 while True:
     img = cam.getImage()
     result = pcf.process(img)
@@ -15,6 +16,10 @@ while True:
         if( result[0].rank is not None and result[0].suit is not None):
             str = "RANK: "+result[0].rank+" SUIT: "+result[0].suit
             img.drawText(str,10,10,color=Color.RED,fontsize=32)
+            #fname = "./captures/Card"+str(count)+".png"
+            s = img.save(temp=True)
+            print s
+            #count = count + 1
     if(img is not None):
         img.show()
     time.sleep(.1)
