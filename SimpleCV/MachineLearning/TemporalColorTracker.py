2361b877111c7aa9c1df2fc9fc7ebf4b1d6c8094
kscottz <kat@sightmachine.com>
2013-05-01 16:36:48 -0400

    find peaks and valleys

--- SimpleCV/MachineLearning/TemporalColorTracker.py
+++ SimpleCV/MachineLearning/TemporalColorTracker.py
@@ -1,5 +1,6 @@
 from SimpleCV import Image, ImageSet, Camera, VirtualCamera, Features, ROI, Color
 import numpy as np
+import scipy.signal as sps
 class TemporalColorTracker:
     def __init__(self):
         self._roi = None # the roi
@@ -20,6 +21,7 @@ class TemporalColorTracker:
         """
         self._extract(src,roi,maxFrames)
         self._findSteadyState()
+        self._findPeaks()
         
     def _getDataFromImg(self,img,roi):
         temp = roi.reassign(img)
@@ -27,31 +29,11 @@ class TemporalColorTracker:
         self.data['r'].append(mc[0])
         self.data['g'].append(mc[1])
         self.data['b'].append(mc[2])
-        # NEED TO CHECK THAT THIS REALL RGB
+        # NEED TO CHECK THAT THIS REALLY RGB
         self.data['i'].append(Color.getLightness(mc))
         self.data['h'].append(Color.getHueFromRGB(mc))
         #return [mc[0],mc[1],mc[2],gray,Color.rgbToHue(mc)]
 
-    def _findSteadyState(self,windowSzPrct=0.05):
-        # slide a window across each of the signals
-        # find where the std dev of the window is minimal
-        # this is the steady state (e.g. where the
-        # assembly line has nothing moving)
-        # save the mean and sd of this value
-        # as a tuple in the steadyStateDict
-        self.steadyState = {}
-        wndwSz = self.data
-        for key in self.data.keys():
-            wndwSz = int(np.floor(windowSzPrct*len(self.data[key])))
-            signal = self.data[key]
-            # slide the window and get the std
-            data = [np.std(signal[i:i+wndwSz]) for i in range(0,len(signal)-wndwSz)]
-            # find the first spot where sd is minimal
-            index = np.where(data==np.min(data))[0][0]
-            # find the mean for the window
-            mean = np.mean(signal[index:index+wndwSz])
-            self.steadyState[key]=(mean,data[index])
-        
     def _extract(self,src,roi,maxFrames):
         self.data = {'r':[],'g':[],'b':[],'i':[],'h':[]}
         if( isinstance(src,ImageSet) ):
@@ -70,3 +52,29 @@ class TemporalColorTracker:
             warnings.warn('Not a valid train source')
             return None
     
+    def _findSteadyState(self,windowSzPrct=0.05):
+        # slide a window across each of the signals
+        # find where the std dev of the window is minimal
+        # this is the steady state (e.g. where the
+        # assembly line has nothing moving)
+        # save the mean and sd of this value
+        # as a tuple in the steadyStateDict
+        self.steadyState = {}
+        for key in self.data.keys():
+            wndwSz = int(np.floor(windowSzPrct*len(self.data[key])))
+            signal = self.data[key]
+            # slide the window and get the std
+            data = [np.std(signal[i:i+wndwSz]) for i in range(0,len(signal)-wndwSz)]
+            # find the first spot where sd is minimal
+            index = np.where(data==np.min(data))[0][0]
+            # find the mean for the window
+            mean = np.mean(signal[index:index+wndwSz])
+            self.steadyState[key]=(mean,data[index])
+
+
+    def _findPeaks(self):
+        self.peaks = {}
+        for key in self.data.keys():
+            wndwSz = int(np.floor(0.05*len(self.data[key])))
+            idxs = sps.find_peaks_cwt(self.data[key],np.arange(1,wndwSz))
+            self.peaks[key] = [(i,self.data[key][i]) for i in idxs]
--- SimpleCV/MachineLearning/TestTemporalColorTracker.py
+++ SimpleCV/MachineLearning/TestTemporalColorTracker.py
@@ -1,5 +1,6 @@
 from SimpleCV import Image, ROI, VirtualCamera, TemporalColorTracker, Color, Display
 import matplotlib.pyplot as plt
+import pickle
 
 cam = VirtualCamera(s='fakedata.avi',st='video')
 img = cam.getImage()
@@ -12,6 +13,7 @@ tct = TemporalColorTracker()
 tct.train(cam, roi,maxFrames=1000)
 plotc = {'r':'r','g':'g','b':'b','i':'m','h':'y'}
 l = len(tct.data['r'])
+pickle.dump(tct,open('tct.pkl','wb'))
 print l
 print tct.steadyState
 for key in tct.data.keys():
@@ -19,6 +21,8 @@ for key in tct.data.keys():
     mu,s = tct.steadyState[key]
     plt.plot([0,l],[mu+3*s,mu+3*s],plotc[key]+'--')
     plt.plot([0,l],[mu-3*s,mu-3*s],plotc[key]+'--')
+    for pt in tct.peaks[key]:
+        plt.plot(pt[0],pt[1],'r.')
     plt.grid()
 plt.show()
 
