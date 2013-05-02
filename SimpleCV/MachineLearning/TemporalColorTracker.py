37610573b3090ba726438d7867078c9a1b4349b3
kscottz <kat@sightmachine.com>
2013-05-02 11:59:37 -0400

    the counting case is done (ish) have a lot of tweaks, want to find classes and add correlation, test

--- SimpleCV/MachineLearning/TemporalColorTracker.py
+++ SimpleCV/MachineLearning/TemporalColorTracker.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.signal as sps
 class TemporalColorTracker:
     def __init__(self):
+        self._rtData = LineScan([]) # the deployed data
         self._roi = None # the roi
         self._steadyState = None # mu/signal for the ss behavior
         self._window = None # the window for local min
@@ -11,7 +12,8 @@ class TemporalColorTracker:
         self._channel = "I" # "R"/"G"/"B"/"I"/"H"
         self._decisionBoundry = None # the threshold for picking values
         
-    def train(self,src,roi,mode="passfail",maxFrames=1000):
+    def train(self,src,roi,mode="passfail",maxFrames=1000,ssWndw=0.05,
+              pkWndw=30, pkDelta=3):
         """
         Use data = video/imageset/camera
         ROI should be ROI feature.
@@ -20,8 +22,11 @@ class TemporalColorTracker:
             counter: returns 'count'/None - e.g. zing
         """
         self._extract(src,roi,maxFrames)
-        self._findSteadyState()
-        self._findPeaks()
+        self._findSteadyState(windowSzPrct=ssWndw)
+        self._findPeaks(pkWndw,pkDelta)
+        self._extractSignalInfo()
+        self._buildSignalProfile()
+        self.roi = roi
         
     def _getDataFromImg(self,img,roi):
         temp = roi.reassign(img)
@@ -45,7 +50,6 @@ class TemporalColorTracker:
                 if( img is None ):
                     break
                 else:
-                    img.show()
                     self._getDataFromImg(img,roi)
                 
         else:
@@ -72,10 +76,96 @@ class TemporalColorTracker:
             self.steadyState[key]=(mean,data[index])
 
 
-    def _findPeaks(self):
+    def _findPeaks(self,pkWndw,pkDelta):
         self.peaks = {}
         self.valleys = {}
         for key in self.data.keys():
             ls = LineScan(self.data[key])
-            self.peaks[key]=ls.findPeaks()
-            self.valleys[key]=ls.findValleys()
+            # need to automagically adjust the window
+            # to make sure we get a minimum number of
+            # of peaks, maybe let the user guess a min?
+            self.peaks[key]=ls.findPeaks(pkWndw,pkDelta)
+            self.valleys[key]=ls.findValleys(pkWndw,pkDelta)
+
+    def _extractSignalInfo(self):
+        self.pD = {}
+        self.vD = {}
+        self.doPeaks = {}
+        bestSpread = 0.00
+        bestDoPeaks = None
+        bestKey = None
+        for key in self.data.keys():
+            #Look at which signal has a bigger distance from
+            #the steady state behavior
+            peakMean = np.mean(np.array(self.peaks[key])[:,1])
+            self.pD[key] =  np.abs(self.steadyState[key][0]-peakMean)
+            valleyMean = np.mean(np.array(self.valleys[key])[:,1])
+            self.vD[key] =  np.abs(self.steadyState[key][0]-valleyMean)
+            self.doPeaks[key]=False
+            best = self.vD[key]
+            if( self.pD[key] > self.vD[key] ):
+                best = self.pD[key]
+                self.doPeaks[key] = True
+            if( best > bestSpread ):
+                bestSpread = best
+                bestDoPeaks = self.doPeaks[key]
+                bestKey = key
+        # Now we know which signal has the most spread
+        # and what direction we are looking for.
+        self.bestKey = key
+        print "Using key " + key
+        
+    def _buildSignalProfile(self):
+        key = self.bestKey
+        peaks = None
+        if( self.doPeaks[key] ):
+            self.isPeak = True
+            peaks = self.peaks[key]
+            # We're just going to do halfway
+            self.cutoff = self.steadyState[key][0]+(self.pD[key]/2.0)            
+        else:
+            self.isPeak = False
+            peaks = self.valleys[key]
+            self.cutoff = self.steadyState[key][0]-(self.vD[key]/2.0)
+        if( len(peaks) > 1 ):
+            p2p = np.array(peaks[1:])-np.array(peaks[:-1])
+            p2pMean = np.mean(p2p)
+            p2pS = np.std(p2p)
+            self.window = p2pMean/2
+        
+        #NEED TO ERROR OUT ON NOT ENOUGH POINTS
+
+    def _getBestValue(self,img):
+        temp = self.roi.reassign(img)
+        mc = temp.meanColor()
+        if( self.bestKey == 'r' ):
+            return mc[0]
+        elif( self.bestKey == 'g' ):
+            return mc[1]
+        elif( self.bestKey == 'b' ):
+            return mc[2]
+        elif( self.bestKey == 'i' ):
+            return Color.getLightness(mc)
+        elif( self.bestKey == 'h' ):
+            return Color.getHueFromRGB(mc)
+        
+    def _updateBuffer(self,v):
+        retVal = None
+        self._rtData.append(v)
+        wndwCenter = int(np.floor(self.window/2.0))
+        # pop the end of the buffer
+        if( len(self._rtData) > self.window):
+            self._rtData = self._rtData[1:]
+            if( self.isPeak ):
+                lm = self._rtData.maxima()[0]
+                if( lm[0] == wndwCenter and lm[1] > self.cutoff ):
+                    retVal = "count"
+            else:
+                lm = self._rtData.minima()[0]
+                if( lm[0] == wndwCenter and lm[1] < self.cutoff ):
+                    retVal = "count"
+        return retVal
+        
+    def recognize(self,img):
+        v = self._getBestValue(img)
+        return self._updateBuffer(v)
--- SimpleCV/MachineLearning/TestTemporalColorTracker.py
+++ SimpleCV/MachineLearning/TestTemporalColorTracker.py
@@ -6,11 +6,10 @@ cam = VirtualCamera(s='fakedata.avi',st='video')
 img = cam.getImage()
 w = img.width
 h = img.height
-disp = Display((1024,768))
 data = []
 roi = ROI(w*0.45,h*0.45,w*0.1,h*0.1,img)
 tct = TemporalColorTracker()
-tct.train(cam, roi,maxFrames=1000)
+tct.train(cam, roi,maxFrames=1200)
 plotc = {'r':'r','g':'g','b':'b','i':'m','h':'y'}
 l = len(tct.data['r'])
 pickle.dump(tct,open('tct.pkl','wb'))
@@ -28,4 +27,25 @@ for key in tct.data.keys():
 
     plt.grid()
 plt.show()
-
+cam = None
+cam = VirtualCamera(s='fakedata.avi',st='video')
+disp = Display((1024,768))
+count = 0
+while disp.isNotDone():
+    img = cam.getImage()
+    result = tct.recognize(img)
+    if( result ):
+        count = count + 1
+        img = img.invert()
+    myStr = "Count: {0}".format(count)
+    plt.plot(tct._rtData,'m-')
+    plt.grid()
+    plt.ylim([0,255])
+    plt.savefig('temp.png')
+    plotImg = Image('temp.png')
+    plt.clf()
+    plotImg = plotImg.scale(0.5)
+    img = img.blit(plotImg,pos=(0,img.height-plotImg.height))
+    img.drawText(myStr,30,30,color=Color.RED,fontsize=64)
+    img = img.applyLayers()
+    img.save(disp)
