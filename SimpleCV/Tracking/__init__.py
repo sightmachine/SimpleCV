7710683f48f60fcb9974a07f174b0ed7ab87054d
jayrambhia <jayrambhia777@gmail.com>
2013-05-08 12:46:39 +0530

    Module names changed
    Signed-off-by: jayrambhia <jayrambhia777@gmail.com>

--- SimpleCV/Tracking/__init__.py
+++ SimpleCV/Tracking/__init__.py
@@ -1,6 +1,6 @@
 from SimpleCV.Tracking.TrackClass import Track, CAMShiftTrack, SURFTrack, LKTrack, MFTrack
-from SimpleCV.Tracking.CAMShift import camshiftTracker
-from SimpleCV.Tracking.lk import lkTracker
-from SimpleCV.Tracking.surfTracker import surfTracker
+from SimpleCV.Tracking.CAMShiftTracker import camshiftTracker
+from SimpleCV.Tracking.LKTracker import lkTracker
+from SimpleCV.Tracking.SURFTracker import surfTracker
 from SimpleCV.Tracking.MFTracker import mfTracker
 from SimpleCV.Tracking.TrackSet import TrackSet
