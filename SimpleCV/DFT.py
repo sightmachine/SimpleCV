f7e64083a5bd799157dd5cf5f1e9c790fc21c530
jayrambhia <jayrambhia777@gmail.com>
2013-05-09 05:38:25 +0530

    lowpass/highpass added
    Signed-off-by: jayrambhia <jayrambhia777@gmail.com>

--- SimpleCV/DFT.py
+++ SimpleCV/DFT.py
@@ -4,11 +4,18 @@ from SimpleCV.ImageClass import Image
 class DFT:
     width = 0
     height = 0
-    _numpy = ""
-    _image = ""
+    _numpy = None
+    _image = None
     _dia = 0
     _type = "None"
     _order = 0
+    _lowpass = False
+    _highpass = False
+    _bandpass = False
+    _xCutoffLow = 0
+    _yCutoffLow = 0
+    _xCutoffHigh = 0
+    _xCutoffHigh = 0
 
     def __init__(self, **kwargs):
         for key in kwargs:
@@ -26,6 +33,20 @@ class DFT:
                 self._type = kwargs[key]
             elif key == 'order':
                 self._order = kwargs[key]
+            elif key == 'lowpass':
+                self._lowpass = kwargs[key]
+            elif key == 'highpass':
+                self._highpass = kwargs[key]
+            elif key == 'bandpass':
+                self._bandpass = kwargs[key]
+            elif key == 'xCutoffLow':
+                self._xCutoffLow = kwargs[key]
+            elif key == 'yCutoffLow':
+                self._yCutoffLow = kwargs[key]
+            elif key == 'xCutoffHigh':
+                self._xCutoffHigh = kwargs[key]
+            elif key == 'yCutoffHigh':
+                self._yCutoffHigh = kwargs[key]
 
     def __repr__(self):
         return "<SimpleCV.DFT Object Filter type: %s, size:(%d, %d)>" %(self._type, self.width, self.height)
@@ -56,6 +77,30 @@ class DFT:
         retVal = DFT(width=sz_x, height=sz_y, numpyarray=flt, image=img, dia=dia, type="Butterworth")
         return retVal
 
+    def createLowpassFilter(self, xCutoff, yCutoff=None, size=(64, 64)):
+        w, h = size
+        xCutoff = np.clip(int(xCutoff*w/2), 0, w/2)
+        if yCutoff is None:
+            yCutoff = xCutoff
+        yCutoff = np.clip(int(yCutoff*h/2), 0, h/2)
+        flt = np.zeros((w, h))
+        flt[0:xCutoff, 0:yCutoff] = 255
+        flt[0:xCutoff, yCutoff-h:h] = 255
+        flt[w-xCutoff:xCutoff, 0:yCutoff] = 255
+        flt[w-xCutoff:w, 0:yCutoff] = 255
+        img = Image(flt)
+        lowpassFilter = DFT(width=w, height=h, numpyarray=flt, image=img, type="Lowpass", xCutoffLow=xCutoff, yCutoffLow=yCutoff)
+        return lowpassFilter
+
+    def createHighpassFilter(self, xCutoff, yCutoff=None, size=(64, 64)):
+        lowpass = self.createLowpassFilter(xCutoff, yCutoff, size)
+        w, h = lowpass.getSize()
+        flt = lowpass._numpy
+        flt = 255 - flt
+        img = Image(flt)
+        highpassFilter = DFT(width=w, height=h, numpyarray=flt, image=img, type="Highpass", xCutoffHigh=xCutoff, yCutoffHigh=yCutoff)
+        return highpassFilter
+
     def applyFilter(self, image):
         if self.width == 0 or self.height == 0:
             warnings.warn("Empty Filter. Returning the image.")
@@ -64,3 +109,38 @@ class DFT:
         fltImg = self._image.resize(w, h)
         filteredImage = image.applyDFTFilter(fltImg)
         return filteredImage
+
+    def getImage(self):
+        if isinstance(self._image, type(None)):
+            if isinstance(self._numpy, type(None)):
+                warnings.warn("Filter doesn't contain any image")
+            self._image = Image(self._numpy)
+        return self._image
+
+    def getNumpy(self):
+        if isinstance(self._numpy, type(None)):
+            if isinstance(self._image, type(None)):
+                warnings.warn("Filter doesn't contain any image")
+            self._numpy = self._image.getNumpy()
+        return self._numpy
+
+    def getOrder(self):
+        return self._order
+
+    def getSize(self):
+        return (self.width, self.height)
+
+    def getDia(self):
+        return self._dia
+
+    def getType(self):
+        return self._type
+
+    def isHighpass(self):
+        return self._highpass
+
+    def isLowpass(self):
+        return self._lowpass
+
+    def isBandpass(self):
+        return self._bandpass
