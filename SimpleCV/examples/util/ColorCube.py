f6a1e67dd62a51438ce2f103f923d39c22b0504f
kscottz <kat@sightmachine.com>
2013-05-09 15:00:38 -0400

    all done.

--- SimpleCV/examples/util/ColorCube.py
+++ SimpleCV/examples/util/ColorCube.py
@@ -1,11 +1,9 @@
-from SimpleCV import Image, Camera, Display
+from SimpleCV import Image, Camera, Display, Color
 import pygame as pg
-import cStringIO
-from pylab import *
 import numpy as np
+from pylab import *
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib.backends.backend_agg import FigureCanvasAgg
-import time as time
 import cv2
 
 bins = 8
@@ -22,12 +20,12 @@ for x in range(0,bins):
             r = ((z*skip)+offset)/255.0
             idxs.append((x,y,z,(r,g,b)))
 
-
-
 # plot points in 3D
-cam = Camera(0)
+cam = Camera()
 disp = Display((800,600))
 fig = figure()
+fig.set_size_inches( (10,7) )
+
 canvas = FigureCanvasAgg(fig)
 azim = 0
 while disp.isNotDone():
@@ -35,31 +33,28 @@ while disp.isNotDone():
     ax.set_xlabel('BLUE', color=(0,0,1) )
     ax.set_ylabel('GREEN',color=(0,1,0))
     ax.set_zlabel('RED',color=(1,0,0))
-    img = cam.getImage().scale(0.2)
+    # Get the color histogram
+    img = cam.getImage().scale(0.3)
     rgb = img.getNumpyCv2()
     hist = cv2.calcHist([rgb],[0,1,2],None,[bins,bins,bins],[0,256,0,256,0,256])
     hist = hist/np.max(hist)
-    print hist.shape
-    #[ ax.plot([x],[y],[z],'.',markersize=hist[x,y,z]*100,color=color) for x,y,z,color in idxs if(hist[x][y][z]>0) ]
-    [ ax.plot([x],[y],[z],'.',color=color) for x,y,z,color in idxs if(hist[x][y][z]>0) ]
-    ax.set_xlim3d(-1, bins)
-    ax.set_ylim3d(-1, bins)
-    ax.set_zlim3d(-1, bins)
-    azim = (azim+0.25)%360
+    # render everything
+    [ ax.plot([x],[y],[z],'.',markersize=max(hist[x,y,z]*100,6),color=color) for x,y,z,color in idxs if(hist[x][y][z]>0) ]
+    #[ ax.plot([x],[y],[z],'.',color=color) for x,y,z,color in idxs if(hist[x][y][z]>0) ]
+    ax.set_xlim3d(0, bins-1)
+    ax.set_ylim3d(0, bins-1)
+    ax.set_zlim3d(0, bins-1)
+    azim = (azim+0.5)%360
     ax.view_init(elev=35, azim=azim)
+    ########### convert matplotlib to  SimpleCV image
     canvas.draw()
     renderer = canvas.get_renderer()
     raw_data = renderer.tostring_rgb()
-    size = canvas.get_width_height()
-    
+    size = canvas.get_width_height()    
     surf = pg.image.fromstring(raw_data, size, "RGB")
     figure = Image(surf)
-#    buf = cStringIO.StringIO()
-#    canvas.print_png(buf)
-    #data = buf.getvalue()
-#    figure = Image(canvas.print_to_buffer())
-#    fig.savefig('temp.png')
-#    figure = Image('temp.png')
-    result = figure.blit(img)
+    ############ All done
+    figure = figure.floodFill((0,0), tolerance=5,color=Color.WHITE)
+    result = figure.blit(img, pos=(20,20))
     result.save(disp)
     fig.clf()
