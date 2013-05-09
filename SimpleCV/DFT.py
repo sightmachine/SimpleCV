7c6e0be8dc111aa8cb379a8a4bfb2e6fd628f4ba
jayrambhia <jayrambhia777@gmail.com>
2013-05-09 17:21:00 +0530

    Docs added
    Signed-off-by: jayrambhia <jayrambhia777@gmail.com>

--- SimpleCV/DFT.py
+++ SimpleCV/DFT.py
@@ -2,6 +2,44 @@ from SimpleCV.base import np, warnings
 from SimpleCV.ImageClass import Image
 
 class DFT:
+    """
+    **SUMMARY**
+
+    The DFT class is the refactored class to crate DFT filters which can
+    be used to filter images by applying Digital Fourier Transform. This
+    is a factory class to create various DFT filters.
+
+    **PARAMETERS**
+
+    Any of the following parameters can be supplied to create 
+    a simple DFT object.
+
+    * *width*        - width of the filter
+    * *height*       - height of the filter
+    * *channels*     - number of channels of the filter
+    * *size*         - size of the filter (width, height)
+    * *_numpy*       - numpy array of the filter
+    * *_image*       - SimpleCV.Image of the filter
+    * *_dia*         - diameter of the filter 
+                      (applicable for gaussian, butterworth, notch)
+    * *_type*        - Type of the filter 
+    * *_order*       - order of the butterworth filter 
+    * *_freqpass*    - frequency of the filter (lowpass, highpass, bandpass)
+    * *_xCutoffLow*  - Lower horizontal cut off frequency for lowpassfilter
+    * *_yCutoffLow*  - Lower vertical cut off frequency for lowpassfilter
+    * *_xCutoffHigh* - Upper horizontal cut off frequency for highpassfilter
+    * *_yCutoffHigh* - Upper vertical cut off frequency for highassfilter
+    
+
+
+    **EXAMPLE**
+
+    >>> gauss = DFT.createGaussianFilter(dia=40, size=(512,512))
+    
+    >>> dft = DFT()
+    >>> butterworth = dft.createButterworthFilter(dia=300, order=2, size=(300, 300))
+
+    """
     width = 0
     height = 0
     channels = 1
@@ -77,6 +115,21 @@ class DFT:
         self._yCutoffHigh = flt._yCutoffHigh
 
     def invert(self):
+        """
+        **SUMMARY**
+
+        Invert the filter. All values will be subtracted from 255.
+
+        **RETURNS**
+
+        Inverted Filter
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createGaussianFilter()
+        >>> invertflt = flt.invert()
+        """
+
         flt = self._numpy
         flt = 255 - flt
         img = Image(flt)
@@ -85,7 +138,36 @@ class DFT:
         invertedfilter._updateParams(self)
         return invertedfilter
 
+    @classmethod
     def createGaussianFilter(self, dia=400, size=(64, 64), highpass=False):
+        """
+        **SUMMARY**
+
+        Creates a gaussian filter of given size.
+
+        **PARAMETERS**
+
+        * *dia*       -  int - diameter of Gaussian filter
+                      - list - provide a list of three diameters to create
+                               a 3 channel filter
+        * *size*      - size of the filter (width, height)
+        * *highpass*: -  bool 
+                         True: highpass filter 
+                         False: lowpass filter
+
+        **RETURNS**
+
+        DFT filter.
+
+        **EXAMPLE**
+
+        >>> gauss = DFT.createGaussianfilter(200, (512, 512),
+                                            highpass=True)
+        >>> gauss = DFT.createGaussianfilter([100, 120, 140], (512, 512),
+                                             highpass=False)
+        >>> img = Image('lenna')
+        >>> gauss.applyFilter(img).show()
+        """
         if isinstance(dia, list):
             if len(dia) != 3 and len(dia) != 1:
                 warnings.warn("diameter list must be of size 1 or 3")
@@ -114,7 +196,37 @@ class DFT:
                      type="Gaussian", frequency=freqpass)
         return retVal
 
+    @classmethod
     def createButterworthFilter(self, dia=400, size=(64, 64), order=2, highpass=False):
+        """
+        **SUMMARY**
+
+        Creates a butterworth filter of given size and order.
+
+        **PARAMETERS**
+
+        * *dia*       - int - diameter of Gaussian filter
+                      - list - provide a list of three diameters to create
+                               a 3 channel filter
+        * *size*      - size of the filter (width, height)
+        * *order*     - order of the filter
+        * *highpass*: -  bool 
+                         True: highpass filter 
+                         False: lowpass filter
+
+        **RETURNS**
+
+        DFT filter.
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createButterworthfilter(100, (512, 512), order=3,
+                                             highpass=True)
+        >>> flt = DFT.createButterworthfilter([100, 120, 140], (512, 512),
+                                             order=3, highpass=False)
+        >>> img = Image('lenna')
+        >>> flt.applyFilter(img).show()
+        """
         if isinstance(dia, list):
             if len(dia) != 3 and len(dia) != 1:
                 warnings.warn("diameter list must be of size 1 or 3")
@@ -143,7 +255,53 @@ class DFT:
                      type="Butterworth", frequency=freqpass)
         return retVal
 
+    @classmethod    
     def createLowpassFilter(self, xCutoff, yCutoff=None, size=(64, 64)):
+        """
+        **SUMMARY**
+
+        Creates a lowpass filter of given size and order.
+
+        **PARAMETERS**
+
+        * *xCutoff*       - int - horizontal cut off frequency
+                          - list - provide a list of three cut off frequencies
+                                   to create a 3 channel filter
+        * *yCutoff*       - int - vertical cut off frequency
+                          - list - provide a list of three cut off frequencies
+                                   to create a 3 channel filter
+        * *size*      - size of the filter (width, height)
+
+        **RETURNS**
+
+        DFT filter.
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=75, size=(320, 280))
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=[75], size=(320, 280))
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=[75, 100, 120],
+                                          size=(320, 280))
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=75, yCutoff=35,
+                                          size=(320, 280))
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=[75], yCutoff=[35],
+                                          size=(320, 280))
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=[75, 100, 125], yCutoff=35,
+                                          size=(320, 280))
+        >>> # yCutoff will be [35, 35, 35]
+
+        >>> flt = DFT.createLowpassFilter(xCutoff=[75, 113, 124],
+                                          yCutoff=[35, 45, 90],
+                                          size=(320, 280))
+        
+        >>> img = Image('lenna')
+        >>> flt.applyFilter(img).show()
+        """
         if isinstance(xCutoff, list):
             if len(xCutoff) != 3 and len(xCutoff) != 1:
                 warnings.warn("xCutoff list must be of size 3 or 1")
@@ -183,7 +341,53 @@ class DFT:
                             yCutoffLow=yCutoff, frequency="lowpass")
         return lowpassFilter
 
+    @classmethod
     def createHighpassFilter(self, xCutoff, yCutoff=None, size=(64, 64)):
+        """
+        **SUMMARY**
+
+        Creates a highpass filter of given size and order.
+
+        **PARAMETERS**
+
+        * *xCutoff*       - int - horizontal cut off frequency
+                          - list - provide a list of three cut off frequencies
+                                   to create a 3 channel filter
+        * *yCutoff*       - int - vertical cut off frequency
+                          - list - provide a list of three cut off frequencies
+                                   to create a 3 channel filter
+        * *size*      - size of the filter (width, height)
+
+        **RETURNS**
+
+        DFT filter.
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=75, size=(320, 280))
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=[75], size=(320, 280))
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=[75, 100, 120],
+                                           size=(320, 280))
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=75, yCutoff=35, 
+                                           size=(320, 280))
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=[75], yCutoff=[35],
+                                           size=(320, 280))
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=[75, 100, 125], yCutoff=35,
+                                           size=(320, 280))
+        >>> # yCutoff will be [35, 35, 35]
+
+        >>> flt = DFT.createHighpassFilter(xCutoff=[75, 113, 124],
+                                           yCutoff=[35, 45, 90],
+                                           size=(320, 280))
+        
+        >>> img = Image('lenna')
+        >>> flt.applyFilter(img).show()
+        """
         if isinstance(xCutoff, list):
             if len(xCutoff) != 3 and len(xCutoff) != 1:
                 warnings.warn("xCutoff list must be of size 3 or 1")
@@ -198,7 +402,8 @@ class DFT:
                 yCutoff = [yCutoff]*len(xCutoff)
             stackedfilter = DFT()
             for xfreq, yfreq in zip(xCutoff, yCutoff):
-                stackedfilter = stackedfilter._stackFilters(self.createHighpassFilter(xfreq, yfreq, size))
+                stackedfilter = stackedfilter._stackFilters(
+                                self.createHighpassFilter(xfreq, yfreq, size))
             image = Image(stackedfilter._numpy)
             retVal = DFT(numpyarray=stackedfilter._numpy, image=image,
                          xCutoffHigh=xCutoff, yCutoffHigh=yCutoff,
@@ -217,7 +422,60 @@ class DFT:
                              yCutoffHigh=yCutoff, frequency="highpass")
         return highpassFilter
 
-    def createBandpassFilter(self, xCutoffLow, xCutoffHigh, yCutoffLow=None, yCutoffHigh=None, size=(64, 64)):
+    @classmethod
+    def createBandpassFilter(self, xCutoffLow, xCutoffHigh, yCutoffLow=None,
+                             yCutoffHigh=None, size=(64, 64)):
+        """
+        **SUMMARY**
+
+        Creates a banf filter of given size and order.
+
+        **PARAMETERS**
+
+        * *xCutoffLow*    - int - horizontal lower cut off frequency
+                          - list - provide a list of three cut off frequencies
+        * *xCutoffHigh*   - int - horizontal higher cut off frequency
+                          - list - provide a list of three cut off frequencies
+        * *yCutoffLow*    - int - vertical lower cut off frequency
+                          - list - provide a list of three cut off frequencies
+        * *yCutoffHigh*   - int - verical higher cut off frequency
+                          - list - provide a list of three cut off frequencies
+                                   to create a 3 channel filter
+        * *size*      - size of the filter (width, height)
+
+        **RETURNS**
+
+        DFT filter.
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=75,
+                                           xCutoffHigh=190, size=(320, 280))
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=[75],
+                                           xCutoffHigh=[190], size=(320, 280))
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=[75, 120, 132],
+                                           xCutoffHigh=[190, 210, 234],
+                                           size=(320, 280))
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=75, xCutoffHigh=190,
+                                           yCutoffLow=60, yCutoffHigh=210,
+                                           size=(320, 280))
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=[75], xCutoffHigh=[190],
+                                           yCutoffLow=[60], yCutoffHigh=[210],
+                                           size=(320, 280))
+
+        >>> flt = DFT.createBandpassFilter(xCutoffLow=[75, 120, 132],
+                                           xCutoffHigh=[190, 210, 234], 
+                                           yCutoffLow=[70, 110, 112], 
+                                           yCutoffHigh=[180, 220, 220], 
+                                           size=(320, 280))
+        
+        >>> img = Image('lenna')
+        >>> flt.applyFilter(img).show()
+        """
         lowpass = self.createLowpassFilter(xCutoffLow, yCutoffLow, size)
         highpass = self.createHighpassFilter(xCutoffHigh, yCutoffHigh, size)
         lowpassnumpy = lowpass._numpy
@@ -232,7 +490,40 @@ class DFT:
                              frequency="bandpass", channels=lowpass.channels)
         return bandpassFilter
 
+    @classmethod
     def createNotchFilter(self, dia1, dia2=None, cen=None, size=(64, 64), type="lowpass"):
+        """
+        **SUMMARY**
+
+        Creates a disk shaped notch filter of given diameter at given center.
+
+        **PARAMETERS**
+
+        * *dia1*       -  int - diameter of the disk shaped notch
+                       - list - provide a list of three diameters to create
+                               a 3 channel filter
+        * *dia2*       -  int - outer diameter of the disk shaped notch
+                                used for bandpass filter
+                       - list - provide a list of three diameters to create
+                               a 3 channel filter
+        * *cen*        - tuple (x, y) center of the disk shaped notch
+                         if not provided, it will be at the center of the 
+                         filter
+        * *size*       - size of the filter (width, height)
+        * *type*:      - lowpass or highpass filter
+
+        **RETURNS**
+        DFT notch filter
+
+        **EXAMPLE**
+
+        >>> notch = DFT.createNotchFilter(dia1=200, cen=(200, 200),
+                                          size=(512, 512), type="highpass")
+        >>> notch = DFT.createNotchFilter(dia1=200, dia2=300, cen=(200, 200),
+                                          size=(512, 512))
+        >>> img = Image('lenna')
+        >>> notch.applyFilter(img).show()
+        """
         if isinstance(dia1, list):
             if len(dia1) != 3 and len(dia1) != 1:
                 warnings.warn("diameter list must be of size 1 or 3")
@@ -294,6 +585,27 @@ class DFT:
         return notchfilter
 
     def applyFilter(self, image):
+        """
+        **SUMMARY**
+
+        Apply the DFT filter to given image.
+
+        **PARAMETERS**
+
+        * *image*  - SimpleCV.Image image
+
+        **RETURNS**
+
+        Filtered Image.
+
+        **EXAMPLE**
+
+        >>> notch = DFT.createNotchFilter(dia1=200, cen=(200, 200),
+                                          size=(512, 512), type="highpass")
+        >>> img = Image('lenna')
+        >>> notch.applyFilter(img).show()
+        """
+
         if self.width == 0 or self.height == 0:
             warnings.warn("Empty Filter. Returning the image.")
             return image
@@ -305,6 +617,21 @@ class DFT:
         return filteredImage
 
     def getImage(self):
+        """
+        **SUMMARY**
+
+        Get the SimpleCV Image of the filter
+
+        **RETURNS**
+
+        Image of the filter.
+
+        **EXAMPLE**
+
+        >>> notch = DFT.createNotchFilter(dia1=200, cen=(200, 200),
+                                          size=(512, 512), type="highpass")
+        >>> notch.getImage().show()
+        """
         if isinstance(self._image, type(None)):
             if isinstance(self._numpy, type(None)):
                 warnings.warn("Filter doesn't contain any image")
@@ -312,6 +639,21 @@ class DFT:
         return self._image
 
     def getNumpy(self):
+        """
+        **SUMMARY**
+
+        Get the numpy array of the filter
+
+        **RETURNS**
+
+        numpy array of the filter.
+
+        **EXAMPLE**
+
+        >>> notch = DFT.createNotchFilter(dia1=200, cen=(200, 200),
+                                          size=(512, 512), type="highpass")
+        >>> notch.getNumpy()
+        """
         if isinstance(self._numpy, type(None)):
             if isinstance(self._image, type(None)):
                 warnings.warn("Filter doesn't contain any image")
@@ -319,18 +661,96 @@ class DFT:
         return self._numpy
 
     def getOrder(self):
+        """
+        **SUMMARY**
+
+        Get order of the butterworth filter
+
+        **RETURNS**
+
+        order of the butterworth filter
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createButterworthFilter(order=4)
+        >>> print flt.getOrder()
+        """
         return self._order
 
     def size(self):
+        """
+        **SUMMARY**
+
+        Get size of the filter
+
+        **RETURNS**
+
+        tuple of (width, height)
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createGaussianFilter(size=(380, 240))
+        >>> print flt.size()
+        """
         return (self.width, self.height)
 
     def getDia(self):
+        """
+        **SUMMARY**
+
+        Get diameter of the filter
+
+        **RETURNS**
+
+        diameter of the filter
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createGaussianFilter(dia=200, size=(380, 240))
+        >>> print flt.getDia()
+        """
         return self._dia
 
     def getType(self):
+        """
+        **SUMMARY**
+
+        Get type of the filter
+
+        **RETURNS**
+
+        type of the filter
+
+        **EXAMPLE**
+
+        >>> flt = DFT.createGaussianFilter(dia=200, size=(380, 240))
+        >>> print flt.getType() # Gaussian
+        """
         return self._type
 
     def stackFilters(self, flt1, flt2):
+        """
+        **SUMMARY**
+
+        Stack three signle channel filters of the same size to create
+        a 3 channel filter.
+
+        **PARAMETERS**
+
+        * *flt1* - second filter to be stacked
+        * *flt2* - thrid filter to be stacked
+
+        **RETURNS**
+
+        DFT filter
+
+        **EXAMPLE**
+
+        >>> flt1 = DFT.createGaussianFilter(dia=200, size=(380, 240))
+        >>> flt2 = DFT.createGaussianFilter(dia=100, size=(380, 240))
+        >>> flt2 = DFT.createGaussianFilter(dia=70, size=(380, 240))
+        >>> flt = flt1.stackFilters(flt2, flt3) # 3 channel filter
+        """
         if not(self.channels == 1 and flt1.channels == 1 and flt2.channels == 1):
             warnings.warn("Filters must have only 1 channel")
             return None
@@ -346,6 +766,20 @@ class DFT:
         return stackedfilter
 
     def _stackFilters(self, flt1):
+        """
+        **SUMMARY**
+
+        stack two filters of same size. channels don't matter.
+
+        **PARAMETERS**
+
+        * *flt1* - second filter to be stacked
+
+        **RETURNS**
+
+        DFT filter
+
+        """
         if isinstance(self._numpy, type(None)):
             return flt1
         if not self.size() == flt1.size():
