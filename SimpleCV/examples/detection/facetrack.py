9539aaf4f7ad334c2f99ff715bad1174ac6c2d84
Fabian Affolter <fabian@affolter-engineering.ch>
2013-07-12 10:08:27 +0200

    License statement added, unused import removed, and updated to match pep8

--- SimpleCV/examples/detection/facetrack.py
+++ SimpleCV/examples/detection/facetrack.py
@@ -1,21 +1,31 @@
-#!/usr/bin/python
-'''
-This program basically does face detection an blurs the face out
-'''
+#!/usr/bin/env python
+# 
+# Released under the BSD license. See LICENSE file for details.
+"""
+This program basically does face detection an blurs the face out.
+"""
 print __doc__
 
-import time
-from SimpleCV import *
+from SimpleCV import Camera, Display, HaarCascade
 
-cam = Camera() #initialize the camera
+# Initialize the camera
+cam = Camera()
 
+# Create the display to show the image
+display = Display()
+
+# Haar Cascade face detection, only faces
 haarcascade = HaarCascade("face")
+
 # Loop forever
-while True:
-    image = cam.getImage().flipHorizontal().scale(0.5)# get image, flip it so it looks mirrored, scale to speed things up
-    faces = image.findHaarFeatures(haarcascade) # load in trained face file
+while display.isNotDone():
+    # Get image, flip it so it looks mirrored, scale to speed things up
+    img = cam.getImage().flipHorizontal().scale(0.5)
+    # Load in trained face file
+    faces = img.findHaarFeatures(haarcascade)
+    # Pixelize the detected face
     if faces:
         bb = faces[-1].boundingBox()
-        image = image.pixelize(10,region=(bb[0],bb[1],bb[2],bb[3]))
-    image.show() #display the image
-
+        img = img.pixelize(10, region=(bb[0], bb[1], bb[2], bb[3]))
+    # Display the image
+    img.save(display)
