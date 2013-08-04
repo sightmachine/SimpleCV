c8eba7ab6e3b54438789cc0b7db6df0e82454c03
jayrambhia <jayrambhia777@gmail.com>
2013-08-04 11:27:53 +0530

    version change
    Signed-off-by: jayrambhia <jayrambhia777@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os, sys, glob, fnmatch
 
 setup(name="SimpleCV",
-  version=1.3,
+  version=2.0,
   download_url='https://github.com/sightmachine/SimpleCV/zipball/1.3',
   description="Make Computers See with SimpleCV, the Python Framework for Machine Vision",
   long_description="""Framework for computer (machine) vision in Python, providing a unified, pythonic interface to image acquisition, conversion, manipulation, and feature extraction.""",
