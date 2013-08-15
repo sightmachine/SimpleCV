ef78bba6c8aafd9df0beac94d15505a16cf89a1e
Rajat Kapoor <rajat100493@gmail.com>
2013-08-15 19:55:15 +0530

    installation fixed

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(name="SimpleCV",
   zip_safe = False,
   requires=['cv2','cv', 'numpy', 'scipy', 'pygame', 'pil'],
   package_data  = { #DO NOT REMOVE, NEEDED TO LOAD INLINE FILES i = Image('simplecv')
-            'SimpleCV': ['sampleimages/*',
+            'SimpleCV': ['Display/Gtk/main.glade',
+                        'sampleimages/*',
                         'Features/HaarCascades/*',
                         'Features/FaceRecognizerData/*'
                         'examples/arduino/*',
