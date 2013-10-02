422cfb1e20d6fca20548a6c9cc3f8259c9c37f76
Anthony Oliver <anthony@sightmachine.com>
2013-10-01 22:19:35 -0400

    Undid the auto scale stuff for notebooks, seemed to have broke the rendering class in notebooks, fixed now

--- SimpleCV/ImageClass.py
+++ SimpleCV/ImageClass.py
@@ -2305,7 +2305,7 @@ class Image:
                     tf = tempfile.NamedTemporaryFile(suffix=".png")
                     loc = tf.name
                     tf.close()
-                    self.scale(fh.autoscalesize).save(loc)
+                    self.save(loc)
                     Idisplay.display(IPImage(filename=loc))
                     return
                 else:
