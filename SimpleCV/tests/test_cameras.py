c7d46e680bad1ed800faa67f51988d8d4273f89d
jason <jxieeducation@live.ca>
2014-03-30 19:53:18 -0700

    camera properties test

--- SimpleCV/tests/test_cameras.py
+++ SimpleCV/tests/test_cameras.py
@@ -41,3 +41,28 @@ def test_camera_multiple_instances():
     if not cam3 or not img3:
         assert False
     pass
+
+def test_camera_properties():
+    mycam = Camera()
+    properties = mycam.getAllProperties()
+    known_properties = {'brightness', 'contrast', 'exposure', 'gain', 'height', 'hue', 'saturation', 'width'}
+    for i in known_properties: #checks to see if all the properties are properly returned
+        if i not in properties:
+            assert False
+
+    for i in properties: #checks to see if getProperty is consistent
+        if mycam.getProperty(i) != properties[i]:
+            assert False
+
+    #checks if the property values are within range
+    if properties['brightness'] < 0 or properties['brightness'] > 1:
+        assert False
+    if properties['contrast'] < 0 or properties['contrast'] > 1:
+        assert False
+    if properties['saturation'] < 0:
+        assert False
+    if properties['height'] < 0:
+        assert False
+    if properties['width'] < 0:
+        assert False
+    pass
